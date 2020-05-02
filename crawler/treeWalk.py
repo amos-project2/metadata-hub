1c6ea06b56895c1a3603463f0044689564afe313
Thomas <tho.wolter@fau.de>
2020-05-02 21:06:58 +0200

    first version of treewalk and hashTable

---
+++ crawler/treeWalk.py
@@ -0,0 +1,67 @@
+#TODO Add logging of already passed directories
+#TODO Add debugging logging
+#TODO Add CPU-Check
+#TODO Add Multi Thread
+
+import os
+import subprocess
+
+def naiveTreeWalk(pathExifTool, pathInput, pathProtocol):
+    """Naive implementation of the tree walk. logs the results in Json format.
+
+    Args:
+        pathExifTool: Path to the exiftool.
+        pathInput: Path to the directory we want to walk.
+        pathProtocol
+
+    Returns:
+        None
+
+    """
+
+    #: Debugging variable to check how many exiftool scans fail
+    failures = []
+
+    #: Walk over every directory and execute the exiftool. Log to file protocol.txt at <pathProtocol>
+    with open(f'{pathProtocol}/protocol.txt', 'w') as myFile:
+        for root, directories, files in os.walk(pathInput):
+            try:
+                subprocess.check_call([f'{pathExifTool}', '-json', root], stdout=myFile)
+            except subprocess.CalledProcessError:
+                failures.append(root)
+
+
+def hashTable(pathInput):
+    """Creates a hash table based on the total amount of files per directory.
+
+    Args:
+        pathExifTool: Path to the exiftool.
+
+    Returns:
+        dictionary with directories maped to amount of files
+
+    """
+    directoryDictionary = dict()
+    for root, directories, files in os.walk(pathInput):
+        if len(files) in directoryDictionary:
+            directoryDictionary[len(files)].append(root)
+        else:
+            directoryDictionary[len(files)] = [root]
+    return directoryDictionary
+
+
+
+# #: For testing (put your on paths into the variables)
+# #: Write paths in Windows with \\ between directorises: C:\\Thomas
+# #: Write paths in Linux with / between directories: /home/thomas
+# #: <pathExifTool>: path to the exiftool.
+# pathExifTool = '/home/thomas/Documents/master/amos/metadata-hub/crawler/exiftool/exiftoolLinux/exiftool'
+# #: <pathInput>: path to folder you want to check
+# pathInput = '/home/thomas/Downloads/TESTY'
+# #: <pathProtocol>:
+# pathProtocol = '/home/thomas/Documents/master/amos/metadata-hub/crawler/protocol'
+# #: for testing purposes
+# naiveTreeWalk(pathExifTool, pathInput, pathProtocol)
+# dictionary = hashTable(pathInput)
+# for entry in dictionary:
+#     print(f'{entry}:  {dictionary[entry]}')
