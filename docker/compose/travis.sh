7ac5b5c838f18e407ec4a2b040ad22ff86b95494
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-05-10 22:47:08 +0200

    Added: Script for building & running in TravisCI Simple bash script that builds and run the services. Starts the services and checks their status after 1 minute.

---
+++ docker/compose/travis.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+function check {
+	RUNNING=$1
+	EXIT=$2
+	NAME=$3
+	if [ "${RUNNING}" == "false" ] && [ "${EXIT}" -ne 0 ]; then
+		echo "${NAME} didn't exit correctly."
+		exit 1
+	fi
+}
+
+TIMEOUT=60
+
+docker-compose build --parallel
+docker-compose up &
+PID=$!
+sleep $TIMEOUT
+
+# CHECK SERVER
+RUNNING_SERVER=$(docker inspect -f {{.State.Running}} metadata-hub.server)
+EXIT_SERVER=$(docker inspect -f {{.State.ExitCode}} metadata-hub.server)
+check "${RUNNING_SERVER}" "${EXIT_SERVER}" "metadata-hub.server"
+
+# CHECK DATABASE
+RUNNING_DATABASE=$(docker inspect -f {{.State.Running}} metadata-hub.database)
+EXIT_DATABASE=$(docker inspect -f {{.State.ExitCode}} metadata-hub.database)
+check "${RUNNING_DATABASE}" "${EXIT_DATABASE}" "metadata-hub.database"
+
+# CHECK CRAWLER
+RUNNING_CRAWLER=$(docker inspect -f {{.State.Running}} metadata-hub.crawler)
+EXIT_CRAWLER=$(docker inspect -f {{.State.ExitCode}} metadata-hub.crawler)
+check "${RUNNING_CRAWLER}" "${EXIT_CRAWLER}" "metadata-hub.crawler"
+
+echo "OK."
