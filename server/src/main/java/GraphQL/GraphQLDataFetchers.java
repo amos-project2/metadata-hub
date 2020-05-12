3b12b660f46fc948b4c579be497d0fda4f0ca4f7
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-05-13 00:28:33 +0200

    Improved: sourended ResultSet with try-with

--- server/src/main/java/GraphQL/GraphQLDataFetchers.java
+++ server/src/main/java/GraphQL/GraphQLDataFetchers.java
@@ -2,11 +2,14 @@ package GraphQL;
 
 import Database.DatabaseProvider;
 import Model.Attribute;
+import Start.Start;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zaxxer.hikari.HikariDataSource;
 import graphql.schema.DataFetcher;
 import lombok.RequiredArgsConstructor;
 import org.jooq.meta.derby.sys.Sys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,6 +20,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class GraphQLDataFetchers
 {
+    private static final Logger log = LoggerFactory.getLogger(GraphQLDataFetchers.class);
 
     private final DatabaseProvider databaseProvider;
 
@@ -26,7 +30,7 @@ public class GraphQLDataFetchers
         {
             final String id = dataFetchingEnvironment.getArgument("id");
 
-            System.out.println(id + " ID");
+            log.info(id + " ID");
             HikariDataSource dataSource = databaseProvider.getHikariDataSource();
 
             try (Connection connection = dataSource.getConnection();
@@ -53,19 +57,21 @@ public class GraphQLDataFetchers
     //Only accesses the file_generic table and extracts the metadata out of the "metadata" field
     public DataFetcher getMetadataFetcher()
     {
-        return (DataFetcher<List<Attribute>>) dataFetchingEnvironment -> {
+        return (DataFetcher<List<Attribute>>) dataFetchingEnvironment ->
+        {
 
             final String file_generic_id = dataFetchingEnvironment.getArgument("file");
             final ArrayList<String> requested_attributes = dataFetchingEnvironment.getArgument("attributes");
             final Boolean eav = dataFetchingEnvironment.getArgument("eav");
 
-            if(eav != null && eav == true){
-               return helperEAVAccess(file_generic_id, requested_attributes);
+            if (eav != null && eav == true)
+            {
+                return helperEAVAccess(file_generic_id, requested_attributes);
             }
 
-            if(requested_attributes != null)
+            if (requested_attributes != null)
             {
-                System.out.println("getMetadata: file_id = " + file_generic_id + " attributes = " + requested_attributes.toString());
+                log.info("getMetadata: file_id = " + file_generic_id + " attributes = " + requested_attributes.toString());
             }
 
             HikariDataSource dataSource = databaseProvider.getHikariDataSource();
@@ -74,89 +80,87 @@ public class GraphQLDataFetchers
                  PreparedStatement selectStmt = connection.prepareStatement("SELECT * from public.file_generic WHERE id=?");)
             {
                 selectStmt.setLong(1, Long.parseLong(file_generic_id));
-                ResultSet rs = selectStmt.executeQuery();
-                if (!rs.next()) return null;
-
-                System.out.println("SQL Result : " + rs.toString());
-                String attribute_id = rs.getString("id");
-                String tree_walk_id = rs.getString("tree_walk_id");
-
-                String jsonFileMetadata = rs.getString("metadata");
-                ObjectMapper mapper = new ObjectMapper();
-                Map<String, String> map = mapper.readValue(jsonFileMetadata, Map.class);
-
-                ArrayList<Attribute> list = new ArrayList<>();
-
-                if(requested_attributes == null)
+                try (ResultSet rs = selectStmt.executeQuery();)
                 {
-                    for(Map.Entry<String, String> entry: map.entrySet())
+                    if (!rs.next()) return null;
+
+                    log.info("SQL Result : " + rs.toString());
+                    String attribute_id = rs.getString("id");
+                    String tree_walk_id = rs.getString("tree_walk_id");
+
+                    String jsonFileMetadata = rs.getString("metadata");
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, String> map = mapper.readValue(jsonFileMetadata, Map.class);
+
+                    ArrayList<Attribute> list = new ArrayList<>();
+
+                    if (requested_attributes == null)
                     {
-                        String key = entry.getKey();
-                        Object value = entry.getValue();
-                        list.add(new Attribute(attribute_id, tree_walk_id, file_generic_id, key, value.toString()));
+                        for (Map.Entry<String, String> entry : map.entrySet())
+                        {
+                            String key = entry.getKey();
+                            Object value = entry.getValue();
+                            list.add(new Attribute(attribute_id, tree_walk_id, file_generic_id, key, value.toString()));
+                        }
                     }
-                }else
+                    else
                     {
-                    for (String attribute : requested_attributes)
-                    {
-                        list.add(new Attribute(attribute_id, tree_walk_id, file_generic_id, attribute, map.get(attribute)));
+                        for (String attribute : requested_attributes)
+                        {
+                            list.add(new Attribute(attribute_id, tree_walk_id, file_generic_id, attribute, map.get(attribute)));
+                        }
                     }
+
+                    return list;
                 }
-
-                return list;
             }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-                return null;
-            }
-
         };
     }
 
-    private List<Attribute> helperEAVAccess(String file_generic_id, List<String> requested_attributes)
+    private List<Attribute> helperEAVAccess(String file_generic_id, List<String> requested_attributes) throws Exception
     {
 
         HikariDataSource dataSource = databaseProvider.getHikariDataSource();
 
+
         StringBuilder sql_statement = new StringBuilder("SELECT * FROM public.file_generic_data_eav WHERE file_generic_id=? ");
-        if(requested_attributes != null)
+        if (requested_attributes != null)
         {
-           sql_statement.append("AND (");
-           for(int i = 0; i+1 < requested_attributes.size(); i++)
-           {
-               sql_statement.append("attribute=? OR ");
-           }
-           sql_statement.append(" attribute=?)");
+            sql_statement.append("AND (");
+            for (int i = 0; i + 1 < requested_attributes.size(); i++)
+            {
+                sql_statement.append("attribute=? OR ");
+            }
+            sql_statement.append(" attribute=?)");
         }
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement selectStmt = connection.prepareStatement(sql_statement.toString());)
         {
             selectStmt.setLong(1, Long.parseLong(file_generic_id));
-            if(requested_attributes != null) {
-                for (int i = 0; i < requested_attributes.size(); i++) {
+            if (requested_attributes != null)
+            {
+                for (int i = 0; i < requested_attributes.size(); i++)
+                {
                     selectStmt.setString(i + 2, requested_attributes.get(i));
                 }
             }
 
-            ResultSet rs = selectStmt.executeQuery();
-            ArrayList<Attribute> list = new ArrayList<>();
-            rs.getFetchSize();
-
-            while(rs.next())
+            try (ResultSet rs = selectStmt.executeQuery();)
             {
-                list.add(new Attribute(rs.getString("id"), rs.getString("tree_walk_id"),
-                    file_generic_id, rs.getString("attribute"), rs.getString("value")));
-            }
+                ArrayList<Attribute> list = new ArrayList<>();
+                rs.getFetchSize();
 
-            return list;
+                while (rs.next())
+                {
+                    list.add(new Attribute(rs.getString("id"), rs.getString("tree_walk_id"),
+                        file_generic_id, rs.getString("attribute"), rs.getString("value")));
+                }
+
+                return list;
+            }
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return null;
-        }
+
     }
 
     public DataFetcher getFileFetcher()
