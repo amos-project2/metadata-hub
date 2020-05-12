6b7ad33e0d26dee00aea829eee35d50fa83e78ab
GregorFendt <gregor.fendt@fau.de>
2020-05-12 21:30:54 +0200

    Added: GraphQL Query that accesses the eav_database

--- server/src/main/java/GraphQL/GraphQLDataFetchers.java
+++ server/src/main/java/GraphQL/GraphQLDataFetchers.java
@@ -57,6 +57,11 @@ public class GraphQLDataFetchers
 
             final String file_generic_id = dataFetchingEnvironment.getArgument("file");
             final ArrayList<String> requested_attributes = dataFetchingEnvironment.getArgument("attributes");
+            final Boolean eav = dataFetchingEnvironment.getArgument("eav");
+
+            if(eav != null && eav == true){
+               return helperEAVAccess(file_generic_id, requested_attributes);
+            }
 
             if(requested_attributes != null)
             {
@@ -107,6 +112,51 @@ public class GraphQLDataFetchers
             }
 
         };
+    }
+
+    private List<Attribute> helperEAVAccess(String file_generic_id, List<String> requested_attributes)
+    {
+
+        HikariDataSource dataSource = databaseProvider.getHikariDataSource();
+
+        StringBuilder sql_statement = new StringBuilder("SELECT * FROM public.file_generic_data_eav WHERE file_generic_id=? ");
+        if(requested_attributes != null)
+        {
+           sql_statement.append("AND (");
+           for(int i = 0; i+1 < requested_attributes.size(); i++)
+           {
+               sql_statement.append("attribute=? OR ");
+           }
+           sql_statement.append(" attribute=?)");
+        }
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectStmt = connection.prepareStatement(sql_statement.toString());)
+        {
+            selectStmt.setLong(1, Long.parseLong(file_generic_id));
+            if(requested_attributes != null) {
+                for (int i = 0; i < requested_attributes.size(); i++) {
+                    selectStmt.setString(i + 2, requested_attributes.get(i));
+                }
+            }
+
+            ResultSet rs = selectStmt.executeQuery();
+            ArrayList<Attribute> list = new ArrayList<>();
+            rs.getFetchSize();
+
+            while(rs.next())
+            {
+                list.add(new Attribute(rs.getString("id"), rs.getString("tree_walk_id"),
+                    file_generic_id, rs.getString("attribute"), rs.getString("value")));
+            }
+
+            return list;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public DataFetcher getFileFetcher()
