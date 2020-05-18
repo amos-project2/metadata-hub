155432e3543bad6bf48ba592a76d073c836dd2cf
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-05-18 20:01:50 +0200

    Updated: Configuration parsing The configuration parsing works now for json data and filepaths. The configuration type definition was updated according to the current schema.

--- crawler/crawler/services/config/typedef.py
+++ crawler/crawler/services/config/typedef.py
@@ -1,22 +1,41 @@
+"""Type definition of a crawler configuration"""
+
+
+# Python imports
+from json import dumps
+
+
 class Config:
+    """Wrapper class for the configuration"""
 
     def __init__(self, data: dict):
         self._data = data
         self._paths = data.get('paths')
         self._options = data.get('options')
 
-    def get_inputs(self):
+    def get_paths_inputs(self) -> list:
         return self._paths.get('inputs')
 
-    def get_output(self):
+    def get_paths_output(self) -> str:
         return self._paths.get('output')
 
-    def get_exiftool(self):
+    def get_paths_exiftool(self) -> str:
         return self._paths.get('exiftool')
 
-    def get_trace(self):
+    def get_paths_trace(self) -> str:
         return self._paths.get('trace')
 
-    def get_options(self):
-        return self._options.get('options')
+    def get_options_power_level(self) -> int:
+        return self._options.get('powerLevel')
 
+    def get_options_clear_trace(self) -> bool:
+            return self._options.get('clearTrace')
+
+    def get_options_file_types(self) -> list:
+            return self._options.get('fileTypes')
+
+    def get_options_language(self) -> str:
+        return self._options.get('language')
+
+    def __str__(self) -> str:
+        return dumps(self._data)
