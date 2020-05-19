42b4f668065c276e4595d54cbca51c8727438a16
GregorFendt <gregor.fendt@fau.de>
2020-05-19 19:02:35 +0200

    Changed: Refactored DataFetchers

--- server/src/main/java/GraphQL/GraphQLDataFetchers.java
+++ server/src/main/java/GraphQL/GraphQLDataFetchers.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 
@@ -65,9 +67,8 @@ public class GraphQLDataFetchers
      * eav: If specified and true, fetches the file metadata from the entity-attribute-value database table
      *
      */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    public DataFetcher getMetadataFetcher()
-    {
+    @SuppressWarnings({"rawtypes"})
+    public DataFetcher getFileMetadataFetcher() {
         return (DataFetcher<List<Attribute>>) dataFetchingEnvironment ->
         {
 
@@ -75,40 +76,14 @@ public class GraphQLDataFetchers
             final ArrayList<String> requested_attributes = dataFetchingEnvironment.getArgument("sel_attributes");
             final Boolean useEAVTable = dataFetchingEnvironment.getArgument("eav");
 
-            if (useEAVTable != null && useEAVTable)
-            {
-                return helperEAVAccess(file_generic_id, requested_attributes);
-            }
-
-            if (requested_attributes != null)
-            {
+            if (requested_attributes != null) {
                 log.info("getMetadata: file_id = " + file_generic_id + " attributes = " + requested_attributes.toString());
             }
 
-            HikariDataSource dataSource = databaseProvider.getHikariDataSource();
-
-            try (Connection connection = dataSource.getConnection();
-                 PreparedStatement selectStmt = connection.prepareStatement("SELECT * from public.file_generic WHERE id=?"))
-            {
-                selectStmt.setLong(1, Long.parseLong(file_generic_id));
-                try (ResultSet rs = selectStmt.executeQuery())
-                {
-                    if (!rs.next()) return null;
-
-                    log.info("SQL Result : " + rs.toString());
-                    String attribute_id = rs.getString("id");
-                    String tree_walk_id = rs.getString("tree_walk_id");
-
-                    String jsonFileMetadata = rs.getString("metadata");
-                    ObjectMapper mapper = new ObjectMapper();
-                    Map<String, String> map = mapper.readValue(jsonFileMetadata, Map.class);
-
-                    ArrayList<Attribute> attributes = new ArrayList<>();
-
-                    helperAddSelAttributes(requested_attributes, attribute_id, tree_walk_id, file_generic_id, attributes, map);
-
-                    return attributes;
-                }
+            if (useEAVTable != null && useEAVTable) {
+                return queryFileMetadataEAV(file_generic_id, requested_attributes);
+            } else {
+                return queryFileMetadata(file_generic_id, requested_attributes);
             }
         };
     }
@@ -124,7 +99,7 @@ public class GraphQLDataFetchers
      * sel_attributes: If specified, fetches solely the selected attributes
      * eav: If specified and true, fetches the file metadata from the entity-attribute-value database table
      */
-    @SuppressWarnings({"rawtypes", "unchecked"})
+    @SuppressWarnings({"rawtypes"})
     public DataFetcher getDirMetadataFetcher()
     {
         return (DataFetcher<List<File>>) dataFetchingEnvironment ->
@@ -134,63 +109,49 @@ public class GraphQLDataFetchers
             final ArrayList<String> selection_attributes = dataFetchingEnvironment.getArgument("sel_attributes");
             final Boolean useEAVTable = dataFetchingEnvironment.getArgument("eav");
 
-            if (useEAVTable != null && useEAVTable)
-            {
-                //TODO Implement EAV Access? Or just stick to the JsonVariant
-                //return helperEAVAccess(dir_path, selection_attributes);
-                return null;
-            }
-
             if (selection_attributes != null)
             {
                 log.info("getDirMetadata: dir_path = " + dir_path + " sel_attributes = " + selection_attributes.toString());
             }
 
-            HikariDataSource dataSource = databaseProvider.getHikariDataSource();
-
-            //SelectStmt = Join on tree_walk_id; Concat the paths and remove the leftmost slash "/home/" + "/testDir/" = "/home/testDir/"
-            try (Connection connection = dataSource.getConnection();
-                 PreparedStatement selectStmt = connection.prepareStatement
-                     ("SELECT *" +
-                         " FROM public.file_generic INNER JOIN public.tree_walk ON public.file_generic.\"tree_walk_Id\" = public.tree_walk.\"id\"" +
-                         " WHERE CONCAT(public.tree_walk.root_path, RIGHT(public.file_generic.sub_dir_path, length(public.file_generic.sub_dir_path) - 1)) LIKE ?"))
+            if (useEAVTable != null && useEAVTable)
             {
-                selectStmt.setString(1, dir_path + "%");
-                try (ResultSet rs = selectStmt.executeQuery())
-                {
-                    ArrayList<File> files = new ArrayList<>();
-                    while(rs.next())
-                    {
-                        //TODO Work on the Model? Right now every query asks for all available information in the database
-                        //TODO and doesn't ask just for the selected File and Attribute Types
-                        //That is the file_id in the database, for the actual attribte_id there would be necessary
-                        // another query to the file_generic_data_eav table
-                        String attribute_id = rs.getString("id");
-                        String tree_walk_id = rs.getString("tree_walk_id");
-                        //TODO File only has relative path as attribute, user right now doesnt get information back about the treewalk
-                        //TODO Right now the user can't calculate the absolute path themselves -> think about which information we send back
-                        //TODO change GraphQL such that it doesn't resemble database scheme but delivers the most useful information to the user?
-                        String absolute_file_path = rs.getString("root_path") + rs.getString("sub_dir_path").substring(1);
-                        String jsonFileMetadata = rs.getString("metadata");
-                        ArrayList<Attribute> attributes = new ArrayList<>();
-                        ObjectMapper mapper = new ObjectMapper();
-                        Map<String, String> attribute_map = mapper.readValue(jsonFileMetadata, Map.class);
-
-                        helperAddSelAttributes(selection_attributes, attribute_id, tree_walk_id, absolute_file_path, attributes, attribute_map);
-                        files.add(new File(attribute_id, tree_walk_id,
-                            absolute_file_path, rs.getString("name"), rs.getString("file_typ"),
-                            rs.getString("file_create_date"), rs.getString("file_modify_date"),
-                            rs.getString("file_access_date"), jsonFileMetadata, attributes));
-
-                    }
-
-                    return files;
-                }
+                //TODO Implement EAV Access? Or just stick to the JsonVariant
+                return queryDirMetadataEAV(dir_path, selection_attributes);
+            }
+            else
+            {
+                return queryDirMetadata(dir_path, selection_attributes);
             }
         };
     }
-    private List<Attribute> helperEAVAccess(String file_generic_id, List<String> requested_attributes) throws Exception
-    {
+    @SuppressWarnings("unchecked")
+    private List<Attribute> queryFileMetadata(String file_generic_id, ArrayList<String> requested_attributes) throws SQLException, IOException {
+        HikariDataSource dataSource = databaseProvider.getHikariDataSource();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectStmt = connection.prepareStatement("SELECT * from public.file_generic WHERE id=?")) {
+            selectStmt.setLong(1, Long.parseLong(file_generic_id));
+            try (ResultSet rs = selectStmt.executeQuery()) {
+                if (!rs.next()) return null;
+
+                log.info("SQL Result : " + rs.toString());
+                String attribute_id = rs.getString("id");
+                String tree_walk_id = rs.getString("tree_walk_id");
+
+                String jsonFileMetadata = rs.getString("metadata");
+                ObjectMapper mapper = new ObjectMapper();
+                Map<String, String> map = mapper.readValue(jsonFileMetadata, Map.class);
+
+                ArrayList<Attribute> attributes = new ArrayList<>();
+
+                helperAddSelAttributes(requested_attributes, attribute_id, tree_walk_id, file_generic_id, attributes, map);
+
+                return attributes;
+            }
+        }
+    }
+    private List<Attribute> queryFileMetadataEAV(String file_generic_id, ArrayList<String> requested_attributes) throws SQLException {
 
         HikariDataSource dataSource = databaseProvider.getHikariDataSource();
 
@@ -233,6 +194,53 @@ public class GraphQLDataFetchers
             }
         }
 
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<File> queryDirMetadata(String dir_path, ArrayList<String> selection_attributes) throws SQLException, IOException {
+        HikariDataSource dataSource = databaseProvider.getHikariDataSource();
+
+        //SelectStmt = Join on tree_walk_id; Concat the paths and remove the leftmost slash "/home/" + "/testDir/" = "/home/testDir/"
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectStmt = connection.prepareStatement
+                 ("SELECT *" +
+                     " FROM public.file_generic INNER JOIN public.tree_walk ON public.file_generic.\"tree_walk_Id\" = public.tree_walk.\"id\"" +
+                     " WHERE CONCAT(public.tree_walk.root_path, RIGHT(public.file_generic.sub_dir_path, length(public.file_generic.sub_dir_path) - 1)) LIKE ?")) {
+            selectStmt.setString(1, dir_path + "%");
+            try (ResultSet rs = selectStmt.executeQuery()) {
+                ArrayList<File> files = new ArrayList<>();
+                while (rs.next()) {
+                    //TODO Work on the Model? Right now every query asks for all available information in the database
+                    //TODO and doesn't ask just for the selected File and Attribute Types
+                    //That is the file_id in the database, for the actual attribte_id there would be necessary
+                    // another query to the file_generic_data_eav table
+                    String attribute_id = rs.getString("id");
+                    String tree_walk_id = rs.getString("tree_walk_id");
+                    //TODO File only has relative path as attribute, user right now doesnt get information back about the treewalk
+                    //TODO Right now the user can't calculate the absolute path themselves -> think about which information we send back
+                    //TODO change GraphQL such that it doesn't resemble database scheme but delivers the most useful information to the user?
+                    String absolute_file_path = rs.getString("root_path") + rs.getString("sub_dir_path").substring(1);
+                    String jsonFileMetadata = rs.getString("metadata");
+                    ArrayList<Attribute> attributes = new ArrayList<>();
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, String> attribute_map = mapper.readValue(jsonFileMetadata, Map.class);
+
+                    helperAddSelAttributes(selection_attributes, attribute_id, tree_walk_id, absolute_file_path, attributes, attribute_map);
+                    files.add(new File(attribute_id, tree_walk_id,
+                        absolute_file_path, rs.getString("name"), rs.getString("file_typ"),
+                        rs.getString("file_create_date"), rs.getString("file_modify_date"),
+                        rs.getString("file_access_date"), jsonFileMetadata, attributes));
+
+                }
+
+                return files;
+            }
+        }
+    }
+
+    private List<File> queryDirMetadataEAV(String dir_path, ArrayList<String> selection_attributes)
+    {
+        return null;
     }
 
     private void helperAddSelAttributes(ArrayList<String> selection_attributes, String attribute_id, String tree_walk_id, String absolute_file_path, ArrayList<Attribute> attributes, Map<String, String> attribute_map) {
