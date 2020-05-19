a94cfe647f3e088d59028aaae6d3bef7805c5e86
GregorFendt <gregor.fendt@fau.de>
2020-05-19 23:16:10 +0200

    Added: GraphQL Metadata Directory Query that uses the EAV table

--- server/src/main/java/GraphQL/GraphQLDataFetchers.java
+++ server/src/main/java/GraphQL/GraphQLDataFetchers.java
@@ -116,7 +116,6 @@ public class GraphQLDataFetchers
 
             if (useEAVTable != null && useEAVTable)
             {
-                //TODO Implement EAV Access? Or just stick to the JsonVariant
                 return queryDirMetadataEAV(dir_path, selection_attributes);
             }
             else
@@ -201,11 +200,18 @@ public class GraphQLDataFetchers
         HikariDataSource dataSource = databaseProvider.getHikariDataSource();
 
         //SelectStmt = Join on tree_walk_id; Concat the paths and remove the leftmost slash "/home/" + "/testDir/" = "/home/testDir/"
-        try (Connection connection = dataSource.getConnection();
+        //TODO Use treewalk table and file table for the absolute path
+        //TODO Or save the absolute path also in the file table
+        /*try (Connection connection = dataSource.getConnection();
              PreparedStatement selectStmt = connection.prepareStatement
                  ("SELECT *" +
-                     " FROM public.file_generic INNER JOIN public.tree_walk ON public.file_generic.\"tree_walk_Id\" = public.tree_walk.\"id\"" +
+                     " FROM public.file_generic INNER JOIN public.tree_walk ON public.file_generic.\"tree_walk_id\" = public.tree_walk.\"id\"" +
                      " WHERE CONCAT(public.tree_walk.root_path, RIGHT(public.file_generic.sub_dir_path, length(public.file_generic.sub_dir_path) - 1)) LIKE ?")) {
+*/
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectStmt = connection.prepareStatement
+                 ("SELECT * FROM public.file_generic " +
+                     "WHERE public.file_generic.sub_dir_path LIKE ?")) {
             selectStmt.setString(1, dir_path + "%");
             try (ResultSet rs = selectStmt.executeQuery()) {
                 ArrayList<File> files = new ArrayList<>();
@@ -219,7 +225,8 @@ public class GraphQLDataFetchers
                     //TODO File only has relative path as attribute, user right now doesnt get information back about the treewalk
                     //TODO Right now the user can't calculate the absolute path themselves -> think about which information we send back
                     //TODO change GraphQL such that it doesn't resemble database scheme but delivers the most useful information to the user?
-                    String absolute_file_path = rs.getString("root_path") + rs.getString("sub_dir_path").substring(1);
+                    //String absolute_file_path = rs.getString("root_path") + rs.getString("sub_dir_path").substring(1);
+                    String absolute_file_path = rs.getString("sub_dir_path");
                     String jsonFileMetadata = rs.getString("metadata");
                     ArrayList<Attribute> attributes = new ArrayList<>();
                     ObjectMapper mapper = new ObjectMapper();
@@ -238,10 +245,64 @@ public class GraphQLDataFetchers
         }
     }
 
-    private List<File> queryDirMetadataEAV(String dir_path, ArrayList<String> selection_attributes)
-    {
-        return null;
+    private List<File> queryDirMetadataEAV(String dir_path, ArrayList<String> selection_attributes) throws SQLException {
+
+        HikariDataSource dataSource = databaseProvider.getHikariDataSource();
+
+/*
+        StringBuilder sql_statement = new StringBuilder("SELECT *" +
+                " FROM public.file_generic" +
+                " INNER JOIN public.tree_walk ON public.file_generic.\"tree_walk_Id\" = public.tree_walk.\"id\"" +
+                " INNER JOIN public.file_generic_data_eav ON public.file_generic.\"tree_walk_id\" = public.file_generic_data_eav.\"tree_walk_id\" " +
+                " WHERE CONCAT(public.tree_walk.root_path, RIGHT(public.file_generic.sub_dir_path, length(public.file_generic.sub_dir_path) - 1)) LIKE ?");
+*/
+        StringBuilder sql_statement = new StringBuilder("SELECT *, public.file_generic.id AS file_id " +
+            "FROM public.file_generic " +
+            "INNER JOIN public.file_generic_data_eav ON public.file_generic.\"tree_walk_id\" = public.file_generic_data_eav.\"tree_walk_id\" " +
+            "WHERE public.file_generic.sub_dir_path LIKE ? ");
+
+        if (selection_attributes != null) {
+            sql_statement.append("AND (");
+            for (int i = 0; i + 1 < selection_attributes.size(); i++) {
+                sql_statement.append("public.file_generic_data_eav.attribute = ? OR ");
+            }
+            sql_statement.append(" public.file_generic_data_eav.attribute=?)");
+        }
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectStmt = connection.prepareStatement(sql_statement.toString())) {
+            selectStmt.setString(1, dir_path + "%");
+            if (selection_attributes != null) {
+                for (int i = 0; i < selection_attributes.size(); i++) {
+                    selectStmt.setString(i + 2, selection_attributes.get(i));
+                }
+            }
+            log.info(selectStmt.toString());
+
+            try (ResultSet rs = selectStmt.executeQuery()) {
+                Map<String, File> files = new HashMap<>();
+
+                while (rs.next()) {
+                    String file_id = rs.getString("file_id");
+                    if (!files.containsKey(file_id)) {
+                        List<Attribute> attributes = new ArrayList<>();
+                        files.put(file_id, new File(file_id, rs.getString("tree_walk_id"), rs.getString("sub_dir_path"),
+                            rs.getString("name"), rs.getString("file_typ"),
+                            rs.getString("file_create_date"), rs.getString("file_modify_date"),
+                            rs.getString("file_access_date"), rs.getString("metadata"), attributes));
+                    }
+
+                    File file = files.get(file_id);
+                    file.getAttributes().add(new Attribute(rs.getString("id"), rs.getString("tree_walk_id"),
+                        file_id, rs.getString("attribute"), rs.getString("value")));
+                }
+
+                return new ArrayList<>(files.values());
+            }
+        }
     }
+
+
 
     private void helperAddSelAttributes(ArrayList<String> selection_attributes, String attribute_id, String tree_walk_id, String absolute_file_path, ArrayList<Attribute> attributes, Map<String, String> attribute_map) {
         if (selection_attributes == null)
