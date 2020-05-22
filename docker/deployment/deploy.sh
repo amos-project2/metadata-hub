c6b7e02bed19bed7023a7e3d3bbf669beb2e5fe3
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-05-22 23:25:15 +0200

    Fixed: Dbus is not working, just run docker logout Don't panic, the credentials themselves are encrypted in the repo. They get decrypted in the VM and now deleted afterwards bc of docker logout. The warning can be ignored.

--- docker/deployment/deploy.sh
+++ docker/deployment/deploy.sh
@@ -4,3 +4,4 @@ id=$(docker images --filter=reference=metadatahub --format "{{.ID}}")
 docker tag $id amosproject2/metadatahub:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push amosproject2/metadatahub
+docker logout
