04dd2bb6aeaea96bb3ff9859820300d6a6d8a711
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-05-24 16:01:35 +0200

    Updated: Deployment on branches develop and master Only deploy the image on branches master and develop with tags latest and dev. On any other branch, just build & run the image.

--- docker/deployment/deploy.sh
+++ docker/deployment/deploy.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-id=$(docker images --filter=reference=metadatahub --format "{{.ID}}")
-docker tag $id amosproject2/metadatahub:latest
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push amosproject2/metadatahub
-docker logout
+branch=$1
+
+# Check if a branch name was passed
+if [ -z "$branch" ]; then
+    echo "No branch was provided. Aborted"
+    exit 1
+fi
+
+if [ $branch != "master" ] && [ $branch != "develop" ]; then
+    # Do not push any feature branches or hotfixes to DockerHub
+    echo "Not pushing branch '$branch' to DockerHub. Done."
+else
+    # Tag and push 'master' or 'develop' to DockerHub
+    image_tag=""
+    if [ $branch == "master" ]; then
+        image_tag="latest"
+    else
+        image_tag="dev"
+    fi
+    id=$(docker images --filter=reference=metadatahub --format "{{.ID}}")
+    echo "Tagging image as '$image_tag'. Pushing to DockerHub"
+    docker tag $id amosproject2/metadatahub:$image_tag
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker push amosproject2/metadatahub
+    docker logout
+    echo "Done."
+fi
