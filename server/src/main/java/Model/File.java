02b8cd3896cdbf2f83425e5caeaa08f5d5c65bf1
GregorFendt <gregor.fendt@fau.de>
2020-06-01 19:01:05 +0200

    Updated: GraphQl model

--- server/src/main/java/Model/File.java
+++ server/src/main/java/Model/File.java
@@ -8,13 +8,14 @@ import java.util.List;
 public class File
 {
     private final String id;
-    private final String tree_walk_id;
-    private final String sub_path;
+    private final String crawl_id;
+    private final String dir_path;
     private final String name;
-    private final String file_type;
-    private final String creation_date;
-    private final String modification_date;
-    private final String access_date;
-    private final String metadataJson;
+    private final String type;
+    private final String size;
     private final List<Metadatum> metadata;
+    private final String creation_time;
+    private final String modification_time;
+    private final String access_time;
+    private final String file_hash;
 }
