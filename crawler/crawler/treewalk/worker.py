4ff498a8f179d23b2222dc946e4e9c7292cacfa0
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-06-06 18:47:09 +0200

    Updated: Worker according to the new Manager One queue for data and commands are used. The last finished worker of an iteration signals the manager that the work is done.

--- crawler/crawler/treewalk/worker.py
+++ crawler/crawler/treewalk/worker.py
@@ -8,15 +8,15 @@ are required for speeding up the execution time.
 
 
 # Python imports
+import json
+import queue
+import hashlib
 import logging
 import subprocess
-import json
-import hashlib
-from typing import List
-from queue import Empty
-from time import sleep # FIXME REMOVE
-from random import random # FIXME REMOVE
-from multiprocessing import Process, Queue
+import time # FIXME
+import random # FIXME
+import multiprocessing
+from typing import List, Any
 
 
 # 3rd party imports
@@ -25,92 +25,68 @@ from psycopg2.extensions import connection
 
 # Local imports
 from crawler.services.config import Config
-from crawler.connectPG.connector import DatabaseConnection
-
+import crawler.database as database
 from crawler.services.tracing import Tracer
+import crawler.communication as communication
+
 
 _logger = logging.getLogger(__name__)
 
 
-class Worker(Process):
+class Worker(multiprocessing.Process):
 
-    COMMAND_STOP = 'stop'
-    COMMAND_PAUSE = 'pause'
-    COMMAND_UNPAUSE = 'unpause'
 
     def __init__(
             self,
-            work_packages: Queue,
-            command_queue: Queue,
+            queue: multiprocessing.Queue,
             config: Config,
-            connectionInfo: dict,
-            # db_connection: DatabaseConnection,
+            connection_data: dict,
             tree_walk_id: int,
-            TRACER: Tracer
+            lock: multiprocessing.Lock,
+            counter: multiprocessing.Value,
+            finished: multiprocessing.Event,
+            num_workers: int
     ):
         super(Worker, self).__init__()
-        self.work_packages = work_packages
-        self.command_queue = command_queue
+        self._queue = queue
         self._config = config
-        self.connectionInfo = connectionInfo
-        # self._db_connection = db_connection
         self._tree_walk_id = tree_walk_id
-        self.TRACER = TRACER
-        self.dbConnectionPool = DatabaseConnection(self.connectionInfo, 1)
-
+        self._lock = lock
+        self._counter = counter
+        self._db_connection = database.DatabaseConnection(connection_data)
+        self._exiftool = self._config.get_exiftool_executable()
+        self._finished = finished
+        self._num_workers = num_workers
 
 
     def run(self) -> None:
         """Run the worker process.
 
-        The worker process consistently checks the command queue in order to
-        receive new commands. Both queues are pulled non-blocking.
-        Commands are dispatched to the run_command method.
-        Work packages are processed as long as the queue is not empty.
-        In this case, all work is done and the process can finish.
+        TODO: Add description
 
         """
-        _logger.info(f'Starting process with PID {self.pid}.')
+        paused = False
+        _logger.info(f'Process {self.pid}: starting.')
         while True:
-            try:
-                command = self.command_queue.get(False)
-                if self.run_command(command):
-                    break
-            except Empty:
-                pass
-            try:
-                package = self.work_packages.get(False)
-            except Empty:
+            command, data = self._queue.get()
+            _logger.info(f'Process {self.pid}: received command {command}.')
+            if (command == communication.WORKER_PACKAGE) and not paused:
+                self._do_work(data)
+            elif command == communication.WORKER_FINISH:
+                self._clean_up()
                 break
-            self._do_work(package)
-        _logger.info(f'Terminating process with PID {self.pid}.')
-
-
-    def run_command(self, command: str) -> bool:
-        """Helper method for running a command retreived by the command queue.
-
-        Args:
-            command (str): command to execute
-
-        Returns:
-            bool: False for continuing, True for stopping
-
-        """
-        _logger.debug(f'Got command {command} for process with PID {self.pid}.')
-        if command == Worker.COMMAND_UNPAUSE:
-            return False
-        if command == Worker.COMMAND_PAUSE:
-            next_command = self.command_queue.get()
-            return self.run_command(next_command)
-        if command == Worker.COMMAND_STOP:
-            self._clean_up()
-            return True
-        # Unknown command was passed. Log this and exit worker process.
-        logging.critical(
-            f'Retrieved invalid command {command}. Terminating {self.pid}.'
-        )
-        self._clean_up()
-        return True
+            elif command == communication.WORKER_STOP:
+                self._clean_up()
+                break
+            elif (command == communication.WORKER_PAUSE) and not paused:
+                paused = True
+            elif (command == communication.WORKER_UNPAUSE) and paused:
+                paused = False
+            else:
+                _logger.error(
+                    f'Process {self.pid}: Ignoring \'{command}\' when paused is {paused}.'
+                )
+        _logger.info(f'Process {self.pid}: terminating.')
 
 
     def createInsert(self, exif: json, value:str) -> str:
@@ -178,19 +154,20 @@ class Worker(Process):
     def _do_work(self, package: List[str]) -> None:
         """Process the work package.
 
+        A work package can either consist of whole directories that are
+        entirely processed or a list of filepaths of a directory that is
+        evenly split across the worker processes.
+
         Args:
-            package (List[str]): list of directories to process.
+            package (List[str]): work package to process
 
         """
-        pathEx = self._config.get_exiftool_executable()
-        _logger.debug(f'Doing work ({self.pid}).')
-
         # for directory in package:
         #     values = f"('{self._tree_walk_id}', '{directory}', name, type, size, metadata)"
         insertin = f"""INSERT INTO files (crawl_id, dir_path, name, type, size, creation_time, access_time, modification_time, metadata, file_hash) """
         value = f"VALUES ('{self._tree_walk_id}', "
         try:
-            process = subprocess.Popen([f'{pathEx}', '-json', *package], stdout=subprocess.PIPE)
+            process = subprocess.Popen([f'{self._exiftool}', '-json', *package], stdout=subprocess.PIPE)
             metadata = json.load(process.stdout)
             for result in metadata:
                 # get the values
@@ -205,25 +182,27 @@ class Worker(Process):
                     bytes = file.read()
                     hash256 = hashlib.sha256(bytes).hexdigest()
                 # insert into the database
-                self.dbConnectionPool.insert_new_record(insertin + values + "'{}'".format(json.dumps(result)) + ', ' + f"'{hash256}'" + ')')
-                self.TRACER.add_node(result['Directory'])
+                self._db_connection.insert_new_record(insertin + values + "'{}'".format(json.dumps(result)) + ', ' + f"'{hash256}'" + ')')
+                # TODO Add directory to analyzed dirs if it was a usual work package
         except Exception as e:
-            print(e)
-
-
+            # print(e)
+            pass
+        # Check if all workers are done
+        # The last one sets the finished event
+        with self._lock:
+            self._counter.value += 1
+            if self._counter.value == self._num_workers:
+                self._counter.value = 0
+                self._finished.set()
+                _logger.debug(f'Process {self.pid}: finished as last.')
 
 
     def _clean_up(self) -> None:
-        """Clean up method for cleaning up all used resources.
-
-        The command queue is already empty.
-
-        """
-        _logger.debug(f'Cleaning up process with PID {self.pid}')
-        self.dbConnectionPool.dbConnectionPool.closeall()
-        # self._db_connection.close()
-
+        """Clean up method for cleaning up all used resources."""
+        _logger.debug(f'Process {self.pid}: cleaning up.')
+        self._db_connection.close()
         # Empty the work package list. Otherwise BrokenPipe errors will appear
         # because the queue still contains items.
-        while not self.work_packages.empty():
-            self.work_packages.get(False)
+        # FIXME: the queue should already actually be empty.
+        while not self._queue.empty():
+            self._queue.get(False)
