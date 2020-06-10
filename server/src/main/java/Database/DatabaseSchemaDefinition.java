34ab3724da33c3316780c4c79833bc2f16502f72
GregorFendt <gregor.fendt@fau.de>
2020-06-10 11:36:44 +0200

    Merge branch 'develop' into java_application
    # Conflicts:
    #	server/src/main/java/Benchmark/BenchmarkTest.java
    #	server/src/main/java/GraphQL/GraphQLDataFetchers.java
    #	server/src/main/java/GraphQL/GraphQLSchemaDefinition.java
    #	server/src/main/java/GraphQL/PreparedStatementCreator.java
    #	server/src/main/java/GraphQL/Provider/Impl/MainGraphQLProviderImpl.java

--- server/src/main/java/Database/DatabaseSchemaDefinition.java
+++ server/src/main/java/Database/DatabaseSchemaDefinition.java
@@ -1,4 +1,24 @@
 package Database;
 
 public class DatabaseSchemaDefinition {
+
+    /**
+     * Columns of the table "files"
+     *
+     * Notice: needs to be specified if we're using multiple tables!
+     */
+    public static final String FILES_TABLE = "files";
+
+    public static final String FILES_ID = "id";
+    public static final String FILES_CRAWL_ID = "crawl_id";
+    public static final String FILES_DIR_PATH = "dir_path";
+    public static final String FILES_NAME = "name";
+    public static final String FILES_TYPE = "type";
+    public static final String FILES_SIZE = "size";
+    public static final String FILES_METADATA = "metadata";
+    public static final String FILES_CREATION_TIME = "creation_time";
+    public static final String FILES_ACCESS_TIME = "access_time";
+    public static final String FILES_MODIFICATION_TIME = "modification_time";
+    public static final String FILES_FILE_HASH = "file_hash";
+
 }
--- server/src/main/java/GraphQL/Model/Crawl.java
+++ server/src/main/java/GraphQL/Model/Crawl.java
@@ -1,4 +1,4 @@
-package Model;
+package GraphQL.Model;
 
 import lombok.Data;
 
