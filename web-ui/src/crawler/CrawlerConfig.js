974c83ce14d2ce0dc3b31f526690afddbb0ffd5e
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-06-11 13:26:41 +0200

    Improved (webui): added in the crawler-config-ui a short description-text

--- web-ui/src/crawler/CrawlerConfig.js
+++ web-ui/src/crawler/CrawlerConfig.js
@@ -11,6 +11,9 @@ export class CrawlerConfig extends Page {
 
     content() {
         return `
+
+    only for developer presentation purposes, the crawler must run to see here something...
+
        <div class="embed-responsive" style="height:calc(100vh - 150px);">
              <iframe class="embed-responsive-item" src="/crawlerui/config" style="height: 100%;"></iframe>
 
