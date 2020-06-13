5acfe9eaea46b1743e47a63ee90138036a82c0db
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-06-14 00:32:54 +0200

    Improved (webui): included logo and changed styling from the loginPage

--- web-ui/src/login/LoginPage.js
+++ web-ui/src/login/LoginPage.js
@@ -1,17 +1,16 @@
 import loginStyles from '../scss/logincss/login.lazy.scss3';
 
 
-
-
 export class LoginPage {
 
     constructor(mountpoint) {
-        this.mountpoint=mountpoint;
+        this.mountpoint = mountpoint;
     }
 
     loadPage() {
         loginStyles.use();
         this.renderIntoMountpoint();
+        $("title").html("Metadata-Hub");
     }
 
     unLoadPage() {
@@ -20,18 +19,21 @@ export class LoginPage {
     }
 
 
-
     //private
     renderIntoMountpoint() {
 
         // language=HTML
-        let data= `
+        let data = `
             <div class="container">
                 <div class="row">
                     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                         <div class="card card-signin my-5">
                             <div class="card-body">
-                                <h5 class="card-title text-center">Sign In</h5>
+                                <div class="text-center" style="width:100%">
+                                    <img src="logo.png" alt="Metadata-Hub" style="width:180px;"> <!--style="width:240px">-->
+                                    <img src="logo.png" alt="Metadata-Hub" style="width:180px;"> <!--style="width:240px">-->
+                                </div>
+                                <h4 class="card-title text-center font-weight-bold">Sign In</h4>
                                 <form class="form-signin">
                                     <div class="form-label-group">
                                         <input type="text" id="your-name" class="form-control" placeholder="Your-Name" required autofocus>
@@ -40,7 +42,7 @@ export class LoginPage {
                                     <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in as Enduser</button>
                                     <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in as Admin</button>
                                     <hr class="my-4">
-                                   * The Your-Name is used for to save it along possible queries you will do. It must be not System-known
+                                    <span class="text-secondary">* The Your-Name is used for save it along possible queries you will do. It must be not System-known.</span>
                                 </form>
                             </div>
                         </div>
