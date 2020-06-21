6a27a515c41189eeb003b3cab560956a8d6c4633
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-06-21 15:01:50 +0200

    Added: Schedule interface in the webUI

--- web-ui/src/crawler/CrawlerScheduler.js
+++ web-ui/src/crawler/CrawlerScheduler.js
@@ -3,66 +3,167 @@ import {Page} from "../Page";
 export class CrawlerScheduler extends Page {
     constructor(parent, identifier, mountpoint, titleSelector) {
         super(parent, identifier, mountpoint, titleSelector);
-        this.title = "Crawler Scheduler";
-        this.restAPIFetcherCrawler=this.parent.dependencies.restApiFetcherCrawler;
+        this.title = "";
+        this.restAPIFetcherCrawler = this.parent.dependencies.restApiFetcherCrawler;
         this.cacheLevel = 3;
-        //use 0 for no caching (the dom for this page will be deleted and onMound/onUnMount is called each enter and leaving the page here)
-        //use 3 for complete caching(the dome for this page stay after leaving
-        //     (its only hidden, be careful, of using html-classes/ids in more areas of the whole webpage, that they dont overlap))
-        //     onMount is only called at the first time, unOnLoad-never (except if you use the delete-cache-method)
-        //     i think using 3 here is a good idea, so the user can work on a formular go to other page and go back to work on
-        //     the dynamic stuff in the page is handled by javascript, timers who reload parts for page in a specific way for specific page-components
-
-
-        //here you set the title-attribut
-        //you can here also set the caching_behavour and much more
-        //take a look in class Page
+        this.updateTimer = null;
     }
 
     content() {
-        return `nothing rigth now`;
-        //here you can return back the html content which should be shown
-    }
+        return `
+            <div class="container">
+                <div class="row mt-3 mb-3">
+                    <div class="col" align="center">
+                        <h1>Schedule</h1>
+                        <form id="remove-id-form">
+                            <div class="form-group row">
+                                <div class="col-12 col-md-3 pt-3">
+                                    <button type="submit" class="btn btn-primary btn-block font-weight-bold">REMOVE</button>
+                                </div>
+                                <div class="col-12 col-md-9 pt-3">
+                                    <input type="text" class="form-control" id="remove-id">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row mt-3 mb-3">
+                    <div id="messages" class="col">
 
-    onMount() {
+                    </div>
+                </div>
+                <div class="row mt-3 mb-3">
+                    <div id="schedule" class="col">
 
-        //its called at caching-level=3 once, and never again (except you call the clearCache-method)
-        //its called at caching-level=0 each time you enter the page and before onLoad-method is called
-
-        //here you can register event-listener
-        //for example: $(".hallo").click(function() {alert("asdf");});
-        //which alerts asdf each time you click on any html element with the class hallo
-
-        //$("#hallo") this here would select not classes but one html-elemnt with id=hallo
-    }
-
-    onUnMount() {
-
-        //its called at caching-level=3 never(except you call the clearCache-method)
-        //its called at caching-level=0 each time you leave the page and after onUnLoad-method is called
-
-        //often you dont need that method, not in both cases i mentioned here
-    }
-
-    onRegister() {
-        //this method is called at the time (and only one time) the whole webapplication is started
-        //often you dont need that method
-    }
-
-    onFirstLoad() {
-        //this method is called on the first-load, while onLoad is called on each load
-
-        //note:
-        //this method could be interessting, if caching-level is 0 and you want to have to run any only one time
-        //and not on each onMount-Time (at caching-level=0)
+                    </div>
+                </div>
+            </div>
+        `;
     }
 
     onLoad() {
-        //this method is called on each load of the page-section here
+        let self = this;
+        self.update();
+        this.updateTimer = setInterval(function () {
+            self.update()
+        }, 60000);
     }
 
     onUnLoad() {
-        //this method is called on each unload of the page-section here
+        clearInterval(this.updateTimer);
+    }
+
+    onMount() {
+        let self = this;
+        $("#remove-id-form").on("submit", function (e) {
+            e.preventDefault();
+            self.removeID(e);
+        });
+    }
+
+
+    update() {
+        let self = this;
+        let schedule = $("#schedule");
+        this.restAPIFetcherCrawler.fetchGet("schedule/list", function (event) {
+            schedule.hide(1000, "swing", function() {
+                schedule.html("");
+                let tasks = event.data.message;
+                let pending = tasks.filter(task => task.pending);
+                let notPending = tasks.filter(task => !task.pending);
+                self.appendSchedule(pending);
+                self.appendSchedule(notPending);
+                schedule.show(1000);
+            });
+        });
+    }
+
+    renderMessage(response) {
+        let status = "Failed!";
+        let alertType = "alert-danger";
+        if (response.success) {
+            status = "Success!";
+            alertType = "alert-success";
+        }
+        return `
+            <div class="alert ${alertType} alert-dismissible fade show" role="alert">
+                <strong>${status}</strong>
+                ${response.message}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        `;
+    }
+
+    removeID(e) {
+        let self = this;
+        let id = $("#remove-id").val();
+        let url = `schedule/remove?id=${id}`;
+        let messages = $("#messages");
+        this.restAPIFetcherCrawler.fetchGet(url, function (event) {
+            let response = event.data;
+            messages.append(self.renderMessage(response));
+            if (response.success) {
+                self.update()
+            }
+            $("#remove-id").val("");
+        });
+    }
+
+    appendSchedule(tasks) {
+        let self = this;
+        let schedule = $("#schedule");
+        let taskHTML = "";
+        tasks.forEach(function(value, index, arr) {
+            taskHTML = self.renderTask(value);
+            schedule.append(taskHTML);
+        });
+    }
+
+    renderTask(task) {
+        console.log(task);
+        let statusBadge = null;
+        let forceBadge = null;
+        if (task.pending) {
+            statusBadge = `<span class="badge badge-warning mr-2">PENDING</span>`;
+        } else {
+            statusBadge = `<span class="badge badge-info mr-2">WAITING</span>`;
+        }
+        if (task.force) {
+            forceBadge = `<span class="badge badge-danger">FORCE</span>`;
+        } else {
+            forceBadge = ``;
+        }
+        return `
+            <div class="card mt-3">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-lg-12 col-xl-10">
+                            ${task.identifier}
+                        </div>
+                        <div class="col-lg-12 col-xl-2">
+                            ${statusBadge}
+                            ${forceBadge}
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        ${task.config.name}
+                    </h5>
+                    <h6 class="card-title text-secondary">
+                        ${task.config.author}
+                    </h6>
+                    <p class="card-text">
+                        ${task.config.description}
+                    </p>
+                    <p class="card-text">
+                        The next execution is scheduled at: <code>${task.timestamp}</code>
+                    </p>
+                </div>
+            </div>
+        `;
     }
 
 }
