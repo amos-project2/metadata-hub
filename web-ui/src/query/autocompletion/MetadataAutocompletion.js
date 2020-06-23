468d71b15a8e3980c7542e8f5ff6fb6e388251ff
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-06-23 21:12:03 +0200

    Added (webui-meta-autocompletion) load-from-server-stubs

--- web-ui/src/query/autocompletion/MetadataAutocompletion.js
+++ web-ui/src/query/autocompletion/MetadataAutocompletion.js
@@ -15,22 +15,20 @@ export class MetadataAutocompletion {
         this.fileTypes = [];
         this.filter = [];
         this.metadata = [];
+
+
+        this.bestMatchingFromServer = [];
+        this.lastFileTypesConcatenated = "";
+
     }
 
-    updateLists() {
+    //private
+    updateListsFilterMetadata() {
 
-        //reset
-        this.fileTypes = [];
         this.filter = [];
         this.metadata = [];
 
-
         let thisdata = this;
-
-        $(this.fileTypesSelector).each(function () {
-            if ($(this).val() === "") return;
-            thisdata.fileTypes.push($(this).val());
-        });
 
         $(this.currentFilterListSelector).each(function () {
             if ($(this).val() === "") return;
@@ -41,23 +39,98 @@ export class MetadataAutocompletion {
             if ($(this).val() === "") return;
             thisdata.metadata.push($(this).val());
         });
+
     }
 
+    //private
+    updateListFileType(callback) {
+
+        //reset
+        this.fileTypes = [];
+
+        let thisdata = this;
+        let tmpConcatenated = "";
+
+
+        $(this.fileTypesSelector).each(function () {
+            if ($(this).val() === "") return;
+            thisdata.fileTypes.push($(this).val());
+            tmpConcatenated += $(this).val();
+        });
+
+        if (tmpConcatenated !== this.lastFileTypesConcatenated) {
+            this.lastFileTypesConcatenated = tmpConcatenated;
+            this.updateServerList(callback);
+        } else {
+            callback();
+        }
+
+    }
+
+
+    //private
+    updateServerList(callback) {
+
+        //stub
+        this.bestMatchingFromServer = ["foo", "bar", "xyz", "usw"];
+        callback();
+    }
+
+    //public
     showLists() {
-        this.updateLists();
+        this.updateListFileType(function () {});
+        this.updateListsFilterMetadata();
         alert(this.fileTypes);
         alert(this.filter);
         alert(this.metadata);
     }
 
 
+    //public
     addListener() {
+        let thisdata = this;
         $(this.modalOpenerSelector).click(function () {
-            $('#metadata-autocompletion-modal').modal();
+            thisdata.openAndConfigureModal();
         });
+        $(".save-metadata-autocompletion").click(function () {
+            thisdata.saveModal();
+        });
+
+    }
+
+    //private
+    openAndConfigureModal() {
+        $(".metadata-autocompletion-suggestions-html").html("Please wait...");
+        this.updateListFileType(function () {
+            $(".metadata-autocompletion-suggestions-html").html("content...");
+        });
+        $('#metadata-autocompletion-modal').modal();
     }
 
 
+    /**
+     * type = 0 -> filter
+     * type = 1 -> metadata
+     */
+    loadSuggestions(type, part, callback) {
+
+        let existing = this.metadata
+        if (type === 0) {
+            existing = this.filter;
+        }
+
+        //load-stub
+        callback(["fileName", "fileSize", "fileType"]);
+    }
+
+
+    saveModal() {
+        //TODO
+        alert("save Modal")
+    }
+
+
+    //public
     getStaticModalHtml() {
 
         return `
@@ -71,7 +144,8 @@ export class MetadataAutocompletion {
                             </button>
                         </div>
                         <div class="modal-body">
-                            Here you automatically add Metadataattributes to the filtersection and metadata-select-section
+                            Here you automatically add Metadataattributes to the filtersection and metadata-select-section.
+                            <hr>
                             <div class="metadata-autocompletion-suggestions-html"></div>
                         </div>
                         <div class="modal-footer">
