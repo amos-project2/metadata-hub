06848c334f64af42704cccd579ded0bca0348319
GregorFendt <gregor.fendt@fau.de>
2020-06-24 10:08:00 +0200

    Deleted: deleted some useless code

--- web-ui/src/query/FormQueryEditor.js
+++ web-ui/src/query/FormQueryEditor.js
@@ -52,7 +52,6 @@ export class FormQueryEditor extends Page {
 
 <!--     date-range-filter           -->
 
-<form class = "needs-validation" novalidate>
                <div class="form-row">
                     <div class="col-md-12">
                         <hr>
@@ -63,16 +62,10 @@ export class FormQueryEditor extends Page {
                     <div class="form-group col-md-6">
                         <label for="fq-createFileTimeRangeStart">Start-DateTime (File created)<a class="pover" title="Start-DateTime" data-content="It collects all files, which are older (created-time) than Start-DateTime">[?]</a></label>
                         <input type="datetime-local" class="form-control" id="fq-createFileTimeRangeStart" placeholder="2020-05-22 07:19:29">
-                        <div class="invalid-feedback">
-                            Start Date can't be after End Date
-                        </div>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="fq-createFileTimeRangeEnd">End-DateTime (File created)<a class="pover" title="End-DateTime" data-content="It collects all files, which are younger (created-time) than End-DateTime">[?]</a></label>
                         <input type="datetime-local" class="form-control" id="fq-createFileTimeRangeEnd" placeholder="2020-07-28 20:35:22">
-                        <div class="invalid-feedback">
-                            End Date can't be before Start Date
-                        </div>
                     </div>
                 </div>
 
@@ -82,19 +75,12 @@ export class FormQueryEditor extends Page {
                     <div class="form-group col-md-6">
                         <label for="fq-createFileTimeRangeStartUpdated">Start-DateTime (File modified)<a class="pover" title="Start-DateTime" data-content="It collects all files, which are older (modified-time) than Start-DateTime">[?]</a></label>
                         <input type="datetime-local" class="form-control" id="fq-createFileTimeRangeStartUpdated" placeholder="2020-05-22 07:19:29">
-                        <div class="invalid-feedback">
-                            Start Date can't be after End Date
-                        </div>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="fq-createFileTimeRangeEndUpdated">End-DateTime (File modified)<a class="pover" title="End-DateTime" data-content="It collects all files, which are younger (modified-time) than End-DateTime">[?]</a></label>
                         <input type="datetime-local" class="form-control" id="fq-createFileTimeRangeEndUpdated" placeholder="2020-07-28 20:35:22">
-                        <div class="invalid-feedback">
-                            End Date can't be before Start Date
-                        </div>
                     </div>
                 </div>
-</form>
 <!--     filetypes filter           -->
 
                <div class="form-row">
@@ -513,12 +499,6 @@ export class FormQueryEditor extends Page {
                 startDateElement.reportValidity();
             }
         })
-        // let startDate = $("#fq-createFileTimeRangeStart").val();
-        // let endDate = $("#fq-createFileTimeRangeEnd").val();
-        //
-        // let startDateUpdated = $("#fq-createFileTimeRangeStartUpdated").val();
-        // let endDateUpdated = $("#fq-createFileTimeRangeEndUpdated").val();
-
 
         //Limit Limit input to integer
         $("#fq-limit").focusout(function(){
