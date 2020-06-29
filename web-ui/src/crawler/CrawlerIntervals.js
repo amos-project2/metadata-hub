caa78a07d0d649e71cf7fcc18c6b7fdcd760612e
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-06-29 13:46:24 +0200

    Added: Time interval configuration panel This panel is used for managing time intervals for maximum resource consumption.

---
+++ web-ui/src/crawler/CrawlerIntervals.js
@@ -0,0 +1,162 @@
+/* Configuration panel for time intervals for maximum resource consumption.
+
+The admin can add/delete time intervals for maximum resource consumption
+using this interface.
+*/
+
+
+import {Page} from "../Page";
+import {Message} from "./components/Message";
+import {IntervalForm} from "./components/IntervalForm";
+import {Interval} from "./components/Interval";
+
+
+export class CrawlerIntervals extends Page {
+    constructor(parent, identifier, mountpoint, titleSelector) {
+        // Page
+        super(parent, identifier, mountpoint, titleSelector);
+        this.title = "";
+        this.cacheLevel = 3;
+        this.restAPIFetcherCrawler = this.parent.dependencies.restApiFetcherCrawler;
+        // Custom
+        this.intervalForm = new IntervalForm();
+        this.timeIntervals = null;
+        this.updateTimer = null;
+        this.hideTimeout = 1000;
+        this.hideTimeoutInstant = 1;
+        this.refreshInterval = 60000;
+    }
+
+    static renderDescription() {
+        return `
+            <p style="text-align: left">
+                This is the configuration panel for the administration of time
+                intervals for maximum resource consumption. These time intervals
+                can be added and deleted using the form and the remove buttons on
+                each item. They are periodically defined for each week.
+                The page is refreshed every <strong>60</strong> or
+                upon adding/deleting time intervals. If you want to update it
+                manually, just click on the tab at the top right corner again.
+            </p>
+        `;
+    }
+
+    content() {
+        return `
+            <div class="container">
+                <div class="row mt-3 mb-3">
+                    <div class="col-12 mb-3" align="center">
+                        <h1>Intervals</h1>
+                        ${CrawlerIntervals.renderDescription()}
+                    </div>
+                    <div class="col-12 mb-2" align="center">
+                        ${this.intervalForm.render()}
+                    </div>
+                </div>
+                <div class="row mt-3 mb-3">
+                    <div id="messages" class="col">
+                    </div>
+                </div>
+                <div class="row mt-3 mb-3">
+                    <div id="intervals" class="col">
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    onMount() {
+        let self = this;
+        $(`#${this.intervalForm.formID}`).on("submit", function (e) {
+            e.preventDefault();
+            self.addInterval();
+        });
+        $(document).on('click', `.${Interval.getClassButtonRemove()}`, function() {
+            let identifier = $(this).data("identifier");
+            self.removeInterval(identifier);
+        });
+    }
+
+    onLoad() {
+        let self = this;
+        this.update(this.hideTimeoutInstant);
+        this.updateTimer = setInterval(function () {
+            self.update(self.hideTimeout)
+        }, this.refreshInterval);
+    }
+
+    onUnLoad() {
+        clearInterval(this.updateTimer);
+        let intervals = $("#intervals");
+        intervals.hide(this.hideTimeoutInstant);
+    }
+
+    update(hideTimeout) {
+        let self = this;
+        let intervals = $("#intervals");
+        this.restAPIFetcherCrawler.fetchGet("intervals/list", function (event) {
+            intervals.hide(hideTimeout, "swing", function() {
+                intervals.html("");
+                let timeIntervals = event.data.message;
+                self.timeIntervals = timeIntervals;
+                self.renderIntervals();
+                intervals.show(self.hideTimeout);
+            });
+        });
+    }
+
+    renderIntervals() {
+        let self = this;
+        let intervals = $("#intervals");
+        let interval = null;
+        self.timeIntervals.forEach(function(value, index, arr) {
+            interval = new Interval(value);
+            intervals.append(interval.render());
+        });
+    }
+
+    addInterval() {
+        let startDay = $(`#${this.intervalForm.startDayID}`).val();
+        let startHours = IntervalForm.parseNumberString(
+            $(`#${this.intervalForm.startHoursID}`).val()
+        );
+        let startMinutes = IntervalForm.parseNumberString(
+            $(`#${this.intervalForm.startMinutesID}`).val()
+        );
+        let endDay = $(`#${this.intervalForm.endDayID}`).val();
+        let endHours = IntervalForm.parseNumberString(
+            $(`#${this.intervalForm.endHoursID}`).val()
+        );
+        let endMinutes = IntervalForm.parseNumberString(
+            $(`#${this.intervalForm.endMinutesID}`).val()
+        );
+        let cpu = $(`#${this.intervalForm.cpuLevelID}`).val();
+        let start = `${startDay}:${startHours}:${startMinutes}`;
+        let end = `${endDay}:${endHours}:${endMinutes}`;
+        let url = `intervals/add?start=${start}&end=${end}&cpu=${cpu}`;
+        let self = this;
+        let messages = $("#messages");
+        self.restAPIFetcherCrawler.fetchGet(url, function (event) {
+            let message = new Message(event.data);
+            messages.append(message.render()).hide().fadeIn(self.hideTimeout);
+            if (message.success) {
+                self.update(self.hideTimeout);
+            }
+        });
+    }
+
+    removeInterval(identifier) {
+        let self = this;
+        let url = `intervals/remove?id=${identifier}`;
+        let messages = $("#messages");
+        this.restAPIFetcherCrawler.fetchGet(url, function (event) {
+            let response = event.data;
+            let message = new Message(response);
+            messages.append(message.render()).hide().fadeIn(self.hideTimeout);
+            if (response.success) {
+                self.update(self.hideTimeout);
+            }
+        });
+    }
+
+}
---
+++ web-ui/src/crawler/components/IntervalForm.js
@@ -0,0 +1,112 @@
+/* Type definition of the form for adding time intervals */
+
+
+export class IntervalForm {
+
+    constructor() {
+        this.formID = "form-interval";
+        this.startDayID = "start-day";
+        this.startHoursID = "start-hours";
+        this.startMinutesID = "start-minutes";
+        this.endDayID = "end-day";
+        this.endHoursID = "end-hours";
+        this.endMinutesID = "end-minutes";
+        this.cpuLevelID = "cpu-level";
+    }
+
+    static parseNumberString(input) {
+        return (input.length === 1) ? `0${input}` : input;
+    }
+
+    static renderInputDay(id) {
+        return `
+            <select class="form-control" id=${id}>
+                <option value="00">Monday</option>
+                <option value="01">Tuesday</option>
+                <option value="02">Wednesday</option>
+                <option value="03">Thursday</option>
+                <option value="04">Friday</option>
+                <option value="05">Saturday</option>
+                <option value="06">Sunday</option>
+            </select>
+        `
+    }
+
+    static renderInputNumber(id, min, max, placeholder) {
+        return `
+            <input
+                type="number"
+                class="form-control"
+                placeholder=${placeholder}
+                id=${id}
+                min=${min}
+                max=${max}
+            >
+        `;
+    }
+
+    static renderTimeInput(id, name) {
+        let idTimeDay = `${id}-day`;
+        let idTimeHours = `${id}-hours`;
+        let idTimeMinutes = `${id}-minutes`;
+        return `
+            <div class="form-group row" id=${id}>
+                <label class="col-sm-3 col-form-label config-input-label">${name}</label>
+                <div class="col-sm-3">
+                    ${IntervalForm.renderInputDay(idTimeDay)}
+                </div>
+                <div class="col-sm-3">
+                    ${IntervalForm.renderInputNumber(idTimeHours, 0, 23, "Hours")}
+                </div>
+                <div class="col-sm-3">
+                    ${IntervalForm.renderInputNumber(idTimeMinutes, 0, 59, "Minutes")}
+                </div>
+            </div>
+        `;
+    }
+
+    static renderCPULevel() {
+        return `
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label config-input-label">CPU Level</label>
+                <div class="col-sm-3">
+                    ${IntervalForm.renderInputNumber("cpu-level", 1, 4, "CPU Level")}
+                </div>
+            </div>
+        `;
+    }
+
+    static renderButtons() {
+        return `
+            <div class="form-group row pt-3">
+                <div class="col-12" align="center">
+                    <button
+                        type="submit"
+                        class="btn btn-primary font-weight-bold mr-3"
+                    >
+                        SUBMIT
+                    </button>
+                    <button
+                        id="clear-config"
+                        type="reset"
+                        class="btn btn-primary font-weight-bold"
+                    >
+                        CLEAR
+                    </button>
+                </div>
+            </div>
+        `;
+    }
+
+    render() {
+        return `
+            <form id=${this.formID}>
+                ${IntervalForm.renderTimeInput("start", "Start")}
+                ${IntervalForm.renderTimeInput("end", "End")}
+                ${IntervalForm.renderCPULevel()}
+                ${IntervalForm.renderButtons()}
+            </form>
+        `;
+    }
+
+}
