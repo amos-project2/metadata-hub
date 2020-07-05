2b91128d8efdf62f2fc227cb49d9cd8922cbd509
GregorFendt <gregor.fendt@fau.de>
2020-07-06 00:46:09 +0200

    Changed: Fixed and Changed the Sorting of the MetadataInfo

--- server/src/main/java/MetadataAutocompletion/MetadataInfoCache.java
+++ server/src/main/java/MetadataAutocompletion/MetadataInfoCache.java
@@ -17,10 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Setter
 public class MetadataInfoCache
 {
-    private List<MetadataInfo> metadataTags = new ArrayList<>();
-    //TODO find a better way of sorting, than a map sorted by its values?
-    private Map<String, MetadataInfo> tagsSorted = new HashMap<>();
-
+    private Map<String, MetadataInfo> tagsSorted = new LinkedHashMap<>();
 
     public MetadataInfoCache(String fileType, Database database)
     {
@@ -37,16 +34,15 @@ public class MetadataInfoCache
 
                 Map<String, ArrayList> metadataMap = new ObjectMapper().readValue(metadataRow, Map.class);
 
-                this.metadataTags = convertToMetadataList(metadataMap);
-                Collections.sort(metadataTags);
-                this.tagsSorted = createSortedMap(this.metadataTags);
-//                this.tagsSorted = this.sortByAttributeOccurence(this.metadataTags);
+                LinkedHashMap<String, MetadataInfo> tmpMap = convertToMetadataInfoMap(metadataMap);
+
+                this.tagsSorted = sortByTagOccurrence(tmpMap);
             }
         }
-        catch (Throwable throwables)
+        catch (Throwable throwable)
         {
-            throwables.printStackTrace();
-            throw new RuntimeException(throwables);
+            throwable.printStackTrace();
+            throw new RuntimeException(throwable);
         }
     }
 
@@ -58,84 +54,46 @@ public class MetadataInfoCache
             metadataInfoCacheList.add(cache.computeIfAbsent(fileType, (key) -> { return new MetadataInfoCache(fileType, database); }));
         }
 
-        //be careful DatabaseSchemaMetadatum is for performance reasons not in-mutable
-        //so a deep-copy is necessary if we want to change the data in another context
-        Map<String, MetadataInfo> tags = new HashMap<>();
+        HashMap<String, MetadataInfo> tags = new HashMap<>();
         for (MetadataInfoCache infoCache : metadataInfoCacheList)
         {
             infoCache.tagsSorted.forEach((tag, metadataInfo) ->
             {
                 tags.compute(tag, (k, metadataInfo2) ->
                 {
-                    //add DatabaseSchemaMetadatum if key not exists
                     if (metadataInfo2 == null) return metadataInfo.copy();
 
-                    //if exists -> merge it
                     return metadataInfo2.merge(metadataInfo);
                 });
             });
         }
 
-        //TODO sort tags and save the result into a list or map
-        this.tagsSorted = tags; //its unsorted //TODO sort it or refactor it to a list
-
-        //TODO notice: metadataTags is empty here
-        //   this.tagsSorted = createSortedMap(this.metadataTags);
-//        this.tagsSorted = this.sortByValue(this.metadataTags);
+        this.tagsSorted = sortByTagOccurrence(tags);
     }
 
-    private List<MetadataInfo> convertToMetadataList(Map<String, ArrayList> metadataMap)
+    /**
+     * converts the database output into a map of MetadataInfo objects with the attribute name as key
+     */
+    private LinkedHashMap<String, MetadataInfo> convertToMetadataInfoMap(Map<String, ArrayList> metadataMap)
     {
-        List<MetadataInfo> metadataList = new ArrayList<>();
+        LinkedHashMap<String, MetadataInfo> metadataInfoMap = new LinkedHashMap<>();
 
         for (Map.Entry<String, ArrayList> entry : metadataMap.entrySet())
         {
-            metadataList.add(new MetadataInfo(entry.getKey(), (Integer) entry.getValue().get(0), MetadatumValueDatatype.valueOf((String) entry.getValue().get(1))));
+            metadataInfoMap.put(entry.getKey(), new MetadataInfo(entry.getKey(), (Integer) entry.getValue().get(0), MetadatumValueDatatype.valueOf((String) entry.getValue().get(1))));
         }
-        return metadataList;
+        return metadataInfoMap;
     }
 
-    private Map<String, MetadataInfo> createSortedMap(List<MetadataInfo> metadataList)
-    {
-        Map<String, MetadataInfo> sortedMap = new LinkedHashMap<>();
-        for (MetadataInfo metadatum : metadataList)
-        {
-            sortedMap.put(metadatum.getAttribute(), metadatum);
-        }
+    private static LinkedHashMap<String, MetadataInfo> sortByTagOccurrence(HashMap<String, MetadataInfo> map) {
+        List<Map.Entry<String, MetadataInfo>> list = new LinkedList<>(map.entrySet());
 
+        Collections.sort(list, Comparator.comparing(Map.Entry::getValue));
+
+        LinkedHashMap<String, MetadataInfo> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, MetadataInfo> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
         return sortedMap;
     }
-
-//    private Map<String,ArrayList> sortByAttributeOccurence(Map<String, ArrayList> metadataMap){
-//
-//        ArrayList<Map.Entry<String, ArrayList>> sortingList = new ArrayList<>(metadataMap.entrySet());
-//        sortingList.sort(comparingByMetadataAttributeOccurence());
-//
-//    }
-//
-//    private static <K, V extends Comparable<? super V>> Comparator< Map.Entry<String, ArrayList> > comparingByMetadataAttributeOccurence() {
-//        return new Comparator<>() {
-//            @Override
-//            public int compare(Map.Entry<String, ArrayList> o1, Map.Entry<String, ArrayList> o2) {
-//                return (Integer)o1.getValue().get(0) - (Integer)o2.getValue().get(0);
-//            }
-//        };
-//    }
-
-    //this method is from here: https://stackoverflow.com/a/2581754/2182302
-//    private <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map)
-//    {
-//        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-//        list.sort(Map.Entry.comparingByValue());
-//
-//        Map<K, V> result = new LinkedHashMap<>();
-//        for (Map.Entry<K, V> entry : list)
-//        {
-//            System.out.println("Entry Val: " + entry.getValue());
-//            result.put(entry.getKey(), entry.getValue());
-//        }
-//
-//        return result;
-//    }
-
 }
