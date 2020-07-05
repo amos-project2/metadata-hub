bf073710ef366bc4daa126054d0e8122fc25fd5c
Benjamin Fischer <benjamin.f.fischer@fau.de>
2020-07-05 20:01:43 +0200

    Updated: Removed DB updater and added DB Threads

--- crawler/main.py
+++ crawler/main.py
@@ -14,6 +14,8 @@ import crawler.services.environment as environment
 import crawler.treewalk.manager as manager
 import crawler.treewalk.scheduler as scheduler
 import crawler.treewalk.db_updater as db_updater
+import crawler.treewalk.db_threads as db_threads
+import crawler.communication as communication
 
 
 if __name__ == '__main__':
@@ -44,23 +46,44 @@ if __name__ == '__main__':
         state=tree_walk_state
     )
     thread_api = threading.Thread(target=api.start, args=(tree_walk_state,))
-    thread_database_updater = db_updater.DatabaseUpdater(
-        db_info=db_connection_data,
-        measure_time=measure_time
-    )
+    # thread_database_updater = db_updater.DatabaseUpdater(
+    #     db_info=db_connection_data,
+    #     measure_time=measure_time
+    # )
     thread_treewalk_scheduler = scheduler.TreeWalkScheduler(
         db_info=db_connection_data,
         measure_time=measure_time,
         update_interval=environment.env.CRAWLER_SCHEDULER_INTERVAL,
         tw_state=tree_walk_state
     )
+    thread_db_files = db_threads.DBThreadFiles(
+        db_info=db_connection_data,
+        measure_time=measure_time,
+        input_data_queue=communication.database_thread_files_input_data,
+        input_command_queue=communication.database_thread_files_input_commands,
+        tw_state=tree_walk_state,
+        update_interval=environment.env.CRAWLER_DB_UPDATE_INTERVAL,
+        is_files_thread=True
+    )
+    thread_db_metadata = db_threads.DBThreadMetadata(
+        db_info=db_connection_data,
+        measure_time=measure_time,
+        input_data_queue=communication.database_thread_metadata_input_data,
+        input_command_queue=communication.database_thread_metadata_input_commands,
+        update_interval=environment.env.CRAWLER_DB_UPDATE_INTERVAL,
+        is_files_thread=False
+    )
     # Starting threads
     thread_api.start()
     thread_treewalk_manager.start()
-    thread_database_updater.start()
+    # thread_database_updater.start()
     thread_treewalk_scheduler.start()
+    thread_db_files.start()
+    thread_db_metadata.start()
     # Joining them on shutdown
     thread_api.join()
+    # thread_database_updater.join()
     thread_treewalk_manager.join()
-    thread_database_updater.join()
     thread_treewalk_scheduler.join()
+    thread_db_files.join()
+    thread_db_metadata.join()
