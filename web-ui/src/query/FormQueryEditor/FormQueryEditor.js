44b9454fcfb2422f18b24342f639c7a49b3c2eaa
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-07-06 18:20:40 +0200

    Refactored (webui-FormQueryEditor) : extracted AdvancedFilter

--- web-ui/src/query/FormQueryEditor/FormQueryEditor.js
+++ web-ui/src/query/FormQueryEditor/FormQueryEditor.js
@@ -3,6 +3,8 @@ import {ResultPresenter} from "../../buisnesslogic/ResultPresenter";
 import {MetadataAutocompletion} from "./autocompletion/MetadataAutocompletion";
 import {InputFieldMultiplier} from "../../buisnesslogic/InputFieldMultiplier";
 import {GraphQlIntrospectionModel} from "./Modals/GraphQlIntrospectionModel";
+import {AdvancedFilter} from "./Components/AdvancedFilter";
+import {FormGraphQl} from "./Components/FormGraphQl";
 
 export class FormQueryEditor extends Page {
     constructor(parent, identifier, mountpoint, titleSelector) {
@@ -11,7 +13,6 @@ export class FormQueryEditor extends Page {
         this.cacheLevel = 3;
         this.graphQlFetcher = this.parent.dependencies.graphQlFetcher;
         this.resultPresenter = new ResultPresenter(this.graphQlFetcher);
-        this.filterFirstElement = this.getFilterElement();
         this.graphQLIntrospectionModal = new GraphQlIntrospectionModel()
 
         this.metadatAutocompletion = new MetadataAutocompletion(
@@ -24,8 +25,10 @@ export class FormQueryEditor extends Page {
             ".modalClearCache"
         );
 
+
+        this.advancedFilter = new AdvancedFilter(this.metadatAutocompletion);
+
         this.inputMultiplierFiletypeFilter = this.inputMultiplierFiletypeFilterBuilder();
-        this.inputMultiplierAdvancedFilterRows = this.inputMultiplierAdvancedFilterRowsBuilder();
         this.inputMultiplierAttributSelector = this.inputMultiplierAttributSelectorBuilder();
 
     }
@@ -53,59 +56,7 @@ export class FormQueryEditor extends Page {
     }
 
 
-    inputMultiplierAdvancedFilterRowsBuilder() {
 
-        let thisdata = this;
-        let emptyFunction = function () {};
-        let appendingHtmlCode = this.getFilterElement();
-
-        let focusOutFunction = function (elem) {
-
-            thisdata.reorderFunctionIdsInFilter();
-            thisdata.metadatAutocompletion.updateLists();
-
-            //Validate Metadata Datatype:
-            // Queries the server to know which kind of datatype the metadatavalue of a given metadata tag has
-            console.log("Tag: " + $(elem).val())
-            if ($(elem).val().length > 1) {
-
-                let datatype = thisdata.metadatAutocompletion.getDataType($(elem).val());
-
-                console.log("datatype: " + datatype)
-
-                if (datatype == "str") {
-                    console.log("datatype str");
-                } else if (datatype == "dig") {
-                    console.log("dig");
-                } else {
-                    console.log("no datatype :(")
-                }
-            }
-        }
-
-        let focusInIfEmptyFieldFunction = function () {
-            thisdata.reorderFunctionIdsInFilter();
-            thisdata.metadatAutocompletion.reAddListener();
-        };
-
-        let additionalListenerFunction = function () {
-            $(".fg-filter-function").not(".listenerAdded").change(function () {
-                if ($(this).val() === "exists") {
-                    $(this).parent().parent().find(".fg-metadata-value").hide();
-                } else {
-                    $(this).parent().parent().find(".fg-metadata-value").show();
-                }
-
-            });
-
-            $(".fg-filter-function").not(".listenerAdded").addClass("listenerAdded");
-        }
-
-
-        return new InputFieldMultiplier(".fg-filter-container", ".fg-metadata-attribute", appendingHtmlCode, emptyFunction,
-            focusOutFunction, focusInIfEmptyFieldFunction, additionalListenerFunction);
-
-    }
 
     inputMultiplierAttributSelectorBuilder() {
 
@@ -209,7 +160,7 @@ export class FormQueryEditor extends Page {
                 </div>
 
 
-                <!--     Advanced-Filter           -->
+
 
 
                <div class="form-row">
@@ -218,49 +169,9 @@ export class FormQueryEditor extends Page {
                     </div>
                 </div>
 
+                <!--     Advanced-Filter           -->
+                ${this.advancedFilter.getMainHtmlCode()}
 
-
-                <div class="form-row">
-                    <div class="col-md-12">Filter: <a class="pover" title="Filter" data-content="Select a filter option.<br>Specify on which metadataattribut you want to use the filter. In the last Input must insert the value<br>For example: Pattern include FileName dog">[?]</a></div>
-                </div>
-
-                <div class="fg-filter-container">
-                    ${this.inputMultiplierAdvancedFilterRows.getFirstElement()}
-                </div>
-                <div>
-                <div class="form-row justify-content-md-center">
-                    <div class="form-group col-md-2">
-                    <label for="fg-filter-connector-options">Filter Connector<a class="pover-filter-connector" style="cursor:pointer; color: #007bff;">[?]</a></label>
-                        <select class="custom-select fg-filter-connector-options" id="fg-filter-connector-options">
-                                <option value="all-and" selected>ALL AND</option>
-                                <option value="all-or">ALL OR</option>
-                                <option value="custom-only">Custom Only</option>
-                                <option value="custom-and">Custom And</option>
-                                <option value="custom-or">Custom OR</option>
-                            </select>
-                    </div>
-                </div>
-
-                 <div class="form-row fq-custom-filter-connector-row-description" style="display:none">
-                 <p class="text-left"><b>Filter-Connector-Description:</b>
-                   <br><br>You can choose here from 5 different filter-connector options. Each option connects your filter in a different way.
-                   <br><b>All AND</b> connects al filter with an AND.
-                   <br><b>ALL OR</b> connects all filter with an OR.
-                   <br><b>Custom Only</b> connects the filter in that way, you want to connect them. So you can connect some filters with an AND some others with an OR, you can also use brackets to group it. If you want to negate a filter you can use a NOT
-                   <br><b>Custom And</b> connects the filter the same way Custum Only does, but appends automatically all not in your custom-input referenced filters with an AND
-                   <br><b>Custom Or</b> connects the filter the same way Custum Only does, but appends automatically all not in your custom-input referenced filters with an OR
-                   <br>
-                   </p>
-                </div>
-
-                 <div class="form-row fq-custom-filter-connector-row" style="display:none">
-                    <div class="form-group col-md-12">
-                        <label for="fq-custom-filter-connector">Custom Filter<a class="pover" title="Custom Filter" data-content="Here you can type in your own bool-expression: Example ((f1 AND f2) OR (f3 AND NOT f0)) AND f5">[?]</a></label>
-                        <input type="text" class="form-control" id="fq-custom-filter-connector" value="">
-                    </div>
-                </div>
-
-                </div>
 
                  <!--     Attribut-Selector           -->
 
@@ -342,8 +253,9 @@ export class FormQueryEditor extends Page {
         $(".resultView1").html(this.resultPresenter.getHtml());
 
         this.inputMultiplierFiletypeFilter.listenerAdd();
-        this.inputMultiplierAdvancedFilterRows.listenerAdd();
         this.inputMultiplierAttributSelector.listenerAdd();
+
+        this.advancedFilter.onMount();
 
         this.inputValidation();
         this.inputSuggestion();
@@ -374,66 +286,6 @@ export class FormQueryEditor extends Page {
             thisdata.clearCache();
             thisdata.reload();
         });
-
-
-        $(".fg-filter-connector-options").change(function () {
-            if ($(this).val().includes("custom")) {
-                thisdata.reorderFunctionIdsInFilter();
-                $(".fq-custom-filter-connector-row").stop(true).show(1000);
-                // $(".function-name-appender").stop(true).show(1000);
-            } else {
-                $(".fq-custom-filter-connector-row").stop(true).hide(1000);
-                $(".function-name-appender").stop(true).hide(1000);
-            }
-        });
-
-        $(".pover-filter-connector").click(function () {
-            $(".fq-custom-filter-connector-row-description").toggle(1000);
-        });
-
-    }
-
-
-    reorderFunctionIdsInFilter() {
-        let countElements = $(".fg-metadata-attribute").length;
-        let counter = 0;
-        let counter2 = -1;
-
-        let customValue = "" + $("#fq-custom-filter-connector").val();
-
-        $(".fg-metadata-attribute").each(function () {
-            counter++;
-            counter2++;
-
-            if (counter == countElements) {
-                $(this).parent().find(".function-name-appender-value").html("not used right now");
-                $(this).parent().find(".function-name-appender").stop(true).hide(1000);
-                return;
-            }
-
-            //reorder function-Ids:
-            let oldValue = $(this).parent().find(".function-name-appender-value").html();
-            if (oldValue !== "f" + counter2) {
-                // customValue = customValue.replaceAll(oldValue, "XXXX" + counter2);
-                customValue = customValue.split(oldValue).join("XXXX" + counter2);
-                customValue = customValue.split("f" + counter2).join("MISSING" + counter2);
-            }
-
-
-            $(this).parent().find(".function-name-appender-value").html("f" + counter2);
-            if ($(".fg-filter-connector-options").val().includes("custom")) {
-                $(this).parent().find(".function-name-appender").stop(true).show(1000);
-            }
-
-        });
-
-        counter2 = -1;
-        $(".fg-metadata-attribute").each(function () {
-            counter2++;
-            // customValue = customValue.replaceAll("XXXX" + counter2, "f"+counter2);
-            customValue = customValue.split("XXXX" + counter2).join("f" + counter2);
-        });
-        $("#fq-custom-filter-connector").val(customValue);
 
     }
 
@@ -519,8 +371,10 @@ export class FormQueryEditor extends Page {
 
     buildAndGetGraphQlQuery() {
 
-        // let filepattern = $("#fq-filePattern").val();
-        //  let checkbox = $("#fq-includeVsExclude").prop('checked');
+        let formGraphQl = new FormGraphQl();
+        this.advancedFilter.generateGraphQlCodeAndSetTo(formGraphQl);
+
+
         let limit = $("#fq-limit").val();
         let showDeleted = $("#fq-showDeleted").prop('checked');
         let deleted = "";
@@ -544,17 +398,12 @@ export class FormQueryEditor extends Page {
         if (startDateUpdated !== "") {startDateUpdated = `start_modification_time: "${startDateUpdated}",\n  `;} else {startDateUpdated = "";}
         if (endDateUpdated !== "") {endDateUpdated = `end_modification_time: "${endDateUpdated}",\n  `;} else {endDateUpdated = "";}
 
-
-        if (filterCustomString !== "" && filterOption.includes("custom")) {filterCustomString = `metadata_filter_logic: "${filterCustomString}",\n  `} else {filterCustomString = "";}
-
-        if (filterOption === "all-and" || filterOption === "custom-and") {
-            filterOption = `metadata_filter_logic_options: and,\n  `;
-        } else if (filterOption === "all-or" || filterOption === "custom-or") {
-            filterOption = `metadata_filter_logic_options: or,\n  `;
-        } else {
-            filterOption = `metadata_filter_logic_options: only_logic_string,\n  `;
-        }
-
+        formGraphQl.limit = limit;
+        formGraphQl.showDeleted = showDeleted;
+        formGraphQl.startDate = startDate;
+        formGraphQl.endDate = endDate;
+        formGraphQl.startDateUpdated = startDateUpdated;
+        formGraphQl.endDateUpdated = endDateUpdated;
 
         let attributes = "";
         {
@@ -581,112 +430,14 @@ export class FormQueryEditor extends Page {
             }
         }
 
-
-        let options_options = "";
-        let options_attributes = "";
-        let options_values = "";
-        {
-            this.inputMultiplierAdvancedFilterRows.each(function (elem) {
-                options_attributes += `"${$(elem).val()}", `;
-                options_options += `${$(elem).parent().find(".fg-filter-function").val()}, `;
-                options_values += `"${$(elem).parent().find(".fg-metadata-value").val()}", `;
-
-            });
-
-            //cause all lists have to have the same size, its ok doing this so
-            if (options_attributes !== "") {
-                options_options = `metadata_options:[${options_options}],\n  `;
-                options_attributes = `metadata_attributes:[${options_attributes}],\n  `;
-                options_values = `metadata_values:[${options_values}],\n  `;
-            }
-        }
-
-//dont change the formatting here, cause this has a direct change to the formatting in the graphql-inspection-window
-        let query_header = `
-   ${limit}
-   ${deleted}
-   ${startDate} ${endDate} ${startDateUpdated} ${endDateUpdated}
-   ${filetypes}
-   ${options_options} ${options_attributes} ${options_values}
-   ${filterOption} ${filterCustomString}
-   ${attributes}
-        `;
-
-        if (query_header.trim() === "") {
-            query_header = "";
-        } else {
-            query_header = `(
-            ${query_header}
-  )`
-        }
-
-        let query = `
-query
-{
-  searchForFileMetadata
-  ${query_header}
-  {
-    id,
-    crawl_id,
-    dir_path,
-    name,
-    type,
-    creation_time,
-    access_time,
-    modification_time,
-    file_hash,
-    deleted,
-    metadata
-    {
-      name,
-      value,
-    }
-  }
-}`;
-//END dont do a change
-
-        return query;
+        formGraphQl.attributes = attributes;
+        formGraphQl.filetypes = filetypes;
 
 
-        // searchForFileMetadata(file_ids: [Int!], crawl_ids: [Int!], dir_path: String, dir_path_option: MetadataOption,
-        //     file_name: String, file_name_option: MetadataOption, file_types: [String!], size: Int, size_option: IntOption,
-        //     start_creation_time: String, end_creation_time: String, start_access_time: String, end_access_time: String,
-        //     start_modification_time: String, end_modification_time: String, file_hashes: [String!],
-        //     metadata_attributes: [String!], metadata_values:[String!], metadata_options: [MetadataOption!],
-        //     metadata_filter_logic_options: FilterLogicOption, metadata_filter_logic: String,
-        //     selected_attributes: [String!], limitFetchingSize: Int, showDeleted: Boolean) : [File]
-
-    }
+        return formGraphQl.generateAndGetGraphQlCode();
 
 
-    getFilterElement() {
 
-        //included, excluded, equal, bigger smaller, exists
-
-        // language=HTML
-        return `
-            <div class="form-row">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <select class="custom-select fg-filter-function">
-                            <option selected value="included">Pattern included</option>
-                            <option value="excluded">Pattern excluded</option>
-                            <option value="equal">Equal</option>
-                            <option value="exists">Exists (Attribute)</option>
-                            <option value="bigger">Greather Than</option>
-                            <option value="smaller">Lower Than</option>
-                        </select>
-                        <div class="input-group-text" style="display:none;">
-                            <input type="checkbox" checked class="fg-include-exclude">
-                        </div>
-                    </div>
-                    <input type="text" class="form-control fg-metadata-attribute" placeholder="Metadata-Attribute">
-                    <input type="text" class="form-control fg-metadata-value" placeholder="Value">
-                    <div class="input-group-append function-name-appender" style="display:none;">
-                        <span class="input-group-text font-weight-bold function-name-appender-value" style="color:red;">f1</span>
-                    </div>
-                </div>
-            </div>`;
     }
 
 
