dd54c89e79307770645c7a1158cc371fee774594
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-07-07 00:08:44 +0200

    Added (webui-ResultPresenter) : Navbar between Json, Table and Export

--- web-ui/src/ResultPresenter/ResultPresenter.js
+++ web-ui/src/ResultPresenter/ResultPresenter.js
@@ -1,4 +1,4 @@
-export class ResultPresenter{
+export class ResultPresenter {
 
     static increaseCount() {
         this.count = this.getCount() + 1;
@@ -10,9 +10,9 @@ export class ResultPresenter{
 
     constructor(graphQlFetcher) {
         ResultPresenter.increaseCount();
-        this.id="ResultPresenter-"+ResultPresenter.getCount();
-        this.pSelector=$("#"+this.id);
-        this.graphQlFetcher=graphQlFetcher;
+        this.id = "ResultPresenter-" + ResultPresenter.getCount();
+        this.pSelector = $("#" + this.id);
+        this.graphQlFetcher = graphQlFetcher;
     }
 
 
@@ -22,15 +22,44 @@ export class ResultPresenter{
         return `
             <div id="${this.id}">
                 <h4>Result:</h4>
-                <div>
-                    <pre class="q_result"></pre>
-                </div>
+
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                      <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="json-tab" data-toggle="tab" href="#json" role="tab" aria-controls="json" aria-selected="true">Json</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#table" role="tab" aria-controls="table" aria-selected="false">Table</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#export" role="tab" aria-controls="export" aria-selected="false">Export</a>
+                      </li>
+                    </ul>
+
+                    <div class="tab-content" id="myTabContent">
+                      <div class="tab-pane fade " id="json" role="tabpanel" aria-labelledby="json-tab">
+
+                        <div>
+                            <pre class="q_result"></pre>
+                        </div>
+
+                      </div>
+                      <div class="tab-pane fade show active" id="table" role="tabpanel" aria-labelledby="table-tab">
+                            ...
+                      </div>
+                      <div class="tab-pane fade" id="export" role="tabpanel" aria-labelledby="export-tab">
+                            ...
+                      </div>
+                    </div>
+
+
+
             </div>`;
 
     }
+
     generateResultAndInjectIntoDom(query) {
-        this.pSelector=$("#"+this.id);//it seems i have to reattach also the beginning of the selector, otherwise it wouldnt work
-        let thisdata=this;
+        this.pSelector = $("#" + this.id);//it seems i have to reattach also the beginning of the selector, otherwise it wouldnt work
+        let thisdata = this;
         this.graphQlFetcher.fetchAdvanced(query, function (sucess, json, jsonString) {
             thisdata.pSelector.find(".q_result").text(jsonString);
         });
