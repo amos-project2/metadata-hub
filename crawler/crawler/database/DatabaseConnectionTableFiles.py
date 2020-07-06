783974ff9fab7bf957a2319189100a8213c58c26
Thomas <tho.wolter@fau.de>
2020-07-06 19:17:23 +0200

    Added: Skeleton for the DatabaseConnectionTableFiles.py

---
+++ crawler/crawler/database/DatabaseConnectionTableFiles.py
@@ -0,0 +1,72 @@
+"""Connection to database and perform query."""
+
+# Python imports
+import json
+import logging
+import os
+from builtins import print
+from datetime import datetime
+from typing import List, Tuple, Dict
+
+# 3rd party modules
+import psycopg2
+from pypika import Query, Table, Field, Parameter
+
+# Local imports
+from crawler.services.config import Config
+import crawler.communication as communication
+
+_logger = logging.getLogger(__name__)
+
+
+def measure_time(func):
+    """Decorator for time measurement of DatabaseConnection objects.
+
+    This decorator is used for roughly estimate the time spent for database
+    operations. It can wrap arbitrary methods of DatabaseConnection objects.
+
+    Args:
+        func (function): function to wrap
+
+    """
+
+    def decorator(self, *args, **kwargs):
+        if self._measure_time:
+            start = datetime.now()
+            result = func(self, *args, **kwargs)
+            end = datetime.now()
+            self._time += (end - start).total_seconds()
+        else:
+            result = func(self, *args, **kwargs)
+        return result
+
+    return decorator
+
+
+class DatabaseConnectionTableFiles:
+
+    def __init__(self, db_info: dict, measure_time: bool) -> None:
+        """Initialize the connection to Postgres Database.
+
+        Args:
+            db_info (dict): connection data of the database
+            measure_time (bool): measure time for database operations
+
+        Raises:
+            VallueError: when creating the connection failed
+
+        """
+
+        try:
+            self.con = psycopg2.connect(
+                user=db_info['user'],
+                password=db_info['password'],
+                host=db_info['host'],
+                port=db_info['port'],
+                database=db_info['dbname']
+            )
+        except Exception as err:
+            raise ValueError(f'Files database initialization error: {err}')
+
+        self._time = 0
+        self._measure_time = measure_time
