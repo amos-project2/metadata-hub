2b46757cf3948202b7990c2429a1085cbfb62d4b
Thomas <tho.wolter@fau.de>
2020-07-07 12:09:46 +0200

    Added: updated __init__

--- crawler/crawler/database/__init__.py
+++ crawler/crawler/database/__init__.py
@@ -1,4 +1,5 @@
 from .connection import DatabaseConnection
 from .connectionFiles import DatabaseConnectionTableFiles
+from .connectionMetadata import DatabaseConnectionTableMetadata
 from .base import measure_time
 from .base import DatabaseConnectionBase
