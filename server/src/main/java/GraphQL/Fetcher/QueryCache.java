f449c64ad7d1e20c87d354a0b9abf916ed52eec5
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-07-08 18:49:27 +0200

    Fixed (javaApplication): bug in the query-cache, add a null check of the selected-attributes

--- server/src/main/java/GraphQL/Fetcher/QueryCache.java
+++ server/src/main/java/GraphQL/Fetcher/QueryCache.java
@@ -11,31 +11,43 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Singleton
-public class QueryCache {
+public class QueryCache
+{
 
     private final ConcurrentHashMap<String, ArrayList<File>> cache = new ConcurrentHashMap<>();
 //    private static final QueryCache queryCache = new QueryCache();
 //    private static final Boolean activatedCacheClearing = queryCache.activateCacheClearing();
 
-    public QueryCache() {
+    public QueryCache()
+    {
         Executors.newScheduledThreadPool(1).scheduleWithFixedDelay(this::clearCache, 10, 10, TimeUnit.MINUTES);
     }
 
-    public ConcurrentHashMap<String, ArrayList<File>> getCache(){
+    public ConcurrentHashMap<String, ArrayList<File>> getCache()
+    {
         return cache;
     }
 
-    public static String createCashKey(String sqlQuery, List<String> selected_attributes){
-        Collections.sort(selected_attributes);
+    public static String createCashKey(String sqlQuery, List<String> selected_attributes)
+    {
         String key = sqlQuery;
-        for(String attribute: selected_attributes){
-           key += attribute;
+
+        if (selected_attributes != null)
+        {
+            Collections.sort(selected_attributes);
+
+
+            for (String attribute : selected_attributes)
+            {
+                key += attribute;
+            }
         }
-       return key;
+        return key;
     }
 
 
-    private void clearCache(){
+    private void clearCache()
+    {
         cache.clear();
     }
 }
