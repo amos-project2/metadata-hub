9545013971261167f49d0b2c90f6342147ff814f
Thomas <tho.wolter@fau.de>
2020-07-12 15:12:39 +0200

    Added: Split create_dict into decrease and increase

--- crawler/crawler/treewalk/worker/utils.py
+++ crawler/crawler/treewalk/worker/utils.py
@@ -2,7 +2,7 @@
 import json
 from typing import Dict, Tuple
 
-def create_metadata_list(exif_output: json) -> Dict:
+def create_metadata_increase(exif_output: json) -> Dict:
     """Creates an easy to process dictionary for updating the 'metadata' table in the database
 
     Args:
@@ -26,6 +26,29 @@ def create_metadata_list(exif_output: json) -> Dict:
                 tag_values[fileType][tag_value][1] = output_type(test[tag_value])
     return tag_values
 
+def create_metadata_decrease(exif_output: json) -> Dict:
+    """Creates an easy to process dictionary for updating the 'metadata' table in the database
+
+    Args:
+        exif_output (json): The output from the ExifTool output.
+    Returns:
+        Dict: key: file type | value: Dict: key: tag value: count
+    """
+    # Loop over every tag for each file in the ExifTool output and add them to the dictionary
+    tag_values = {}
+    for single_output in exif_output:
+        fileType = single_output['FileType']
+        if fileType not in tag_values:
+            tag_values[fileType] = {}
+        for tag_value in single_output:
+            test = dict(single_output)
+            if tag_value in tag_values[fileType]:
+                tag_values[fileType][tag_value][0] -= 1
+            else:
+                tag_values[fileType][tag_value] = [-1, '?']
+            if tag_values[fileType][tag_value][1] == '?':
+                tag_values[fileType][tag_value][1] = output_type(test[tag_value])
+    return tag_values
 
 def output_type(to_check: str):
     """Determine whether the output value of a file is a digit or a string
