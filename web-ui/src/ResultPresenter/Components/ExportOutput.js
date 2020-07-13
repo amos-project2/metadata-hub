420b55926108f9e6da8e637a5c7a5737981f5b9b
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-07-13 17:35:19 +0200

    Added (webui-visualiziation): export-ui-elements

--- web-ui/src/ResultPresenter/Components/ExportOutput.js
+++ web-ui/src/ResultPresenter/Components/ExportOutput.js
@@ -1,16 +1,54 @@
 export class ExportOutput {
 
+    static increaseCount() {
+        this.count = this.getCount() + 1;
+        return this.count;
+    }
+
+    static getCount() {
+        return this.count || 0;
+    }
 
     constructor() {
-
+        this.id = "export-" + ExportOutput.increaseCount();
+        this.pSelector = null;
     }
 
     getMainHtmlCode() {
-        return `coming soon`;
+        // language=HTML
+        return `
+            <hr>
+            <br>
+            <div class="mx-auto" style="width: 290px;">
+                <div class="form-check mb-4" id="${this.id}">
+                    <input class="form-check-input" type="checkbox" value="" id="meta-all${this.id}">
+                    <label class="form-check-label" for="meta-all${this.id}">
+                        Include all Metadataattributes <br>(Ignore the Metadataattributes-Filter)
+                    </label>
+                </div>
+                 <div class="form-check mb-4" id="${this.id}">
+                    <input class="form-check-input" type="checkbox" value="" id="pagination-no${this.id}">
+                    <label class="form-check-label" for="pagination-no${this.id}">
+                        Include all rows <br>(Ignore pagination)
+                    </label>
+                </div>
+                <div class="form-check mb-4" id="${this.id}">
+                    <input class="form-check-input" type="checkbox" value="" id="query-include${this.id}">
+                    <label class="form-check-label" for="query-include${this.id}">
+                       Include the query
+                    </label>
+                </div>
+                <div class="col text-center mb-4">
+                    <button type="button" class="btn btn-success ">Download</button>
+                </div>
+            </div>
+            <br>
+            <hr>`;
     }
 
-    onMount(pSelector) {
-        this.pSelector=pSelector;
+    onMount(pSelectorParent) {
+        this.pSelector = $("#" + this.id);
     }
+
 
 }
