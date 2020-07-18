a5935ed576de4c8f50d3b02827b037411a81f315
GregorFendt <gregor.fendt@fau.de>
2020-07-18 16:07:02 +0200

    Changed: Spelling in the UI

--- web-ui/src/about/License.js
+++ web-ui/src/about/License.js
@@ -17,7 +17,7 @@ export class License extends Page {
                 <pre>
 MIT License
 
-Copyright (c) 2020 Benjamin Fischer, Thomas Wolter, Robin Kreuzer, Ngoc Khang Nguyen
+Copyright (c) 2020 Benjamin Fischer, Thomas Wolter, Robin Kreuzer, Ngoc Khang Nguyen, Gregor Fendt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
