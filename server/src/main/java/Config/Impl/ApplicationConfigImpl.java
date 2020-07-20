9657a3be22cc68006d1b14693a84ae3453dc8394
Robin Kreuzer <Robin-Kreuzer@gmx.de>
2020-07-20 22:24:26 +0200

    Added (java-application) the missing config-checks

--- server/src/main/java/Config/Impl/ApplicationConfigImpl.java
+++ server/src/main/java/Config/Impl/ApplicationConfigImpl.java
@@ -143,6 +143,37 @@ public class ApplicationConfigImpl implements ApplicationConfig
         if (this.config.getProperty("server-port") == null) this.errorMessage = "Property server-port is missing";
 
 
+
+        {
+            String tmp = this.config.getProperty("server-logging-level");
+            if (tmp == null) tmp = "";
+            tmp = tmp.toLowerCase().trim();
+
+            if (!(tmp.equals("trace") || tmp.equals("debug") || tmp.equals("error") || tmp.equals("warn") || tmp.equals("info"))) this.errorMessage = "Property server-logging-level must contain warn, error, trace, debug or info";
+        }
+
+        if (this.config.getProperty("server-logging-level") == null) this.errorMessage = "Property server-logging-level is missing";
+
+        if (this.config.getProperty("webui-queryConstructorEnabled") == null) this.errorMessage = "Property webui-queryConstructorEnabled is missing";
+        if (this.checkBooleanValue(this.config.getProperty("webui-queryConstructorEnabled"))) this.errorMessage = "Property webui-queryConstructorEnabled must be a boolean";
+
+        if (this.config.getProperty("webui-crawlerEnabled") == null) this.errorMessage = "Property webui-crawlerEnabled is missing";
+        if (this.checkBooleanValue(this.config.getProperty("webui-crawlerEnabled"))) this.errorMessage = "Property webui-crawlerEnabled must be a boolean";
+
+        if (this.config.getProperty("webui-defaultUsername") == null) this.errorMessage = "Property webui-defaultUsername is missing";
+
+        if (this.config.getProperty("webui-adminLoginEnabeled") == null) this.errorMessage = "Property webui-adminLoginEnabeled is missing";
+        if (this.checkBooleanValue(this.config.getProperty("webui-adminLoginEnabeled"))) this.errorMessage = "Property webui-adminLoginEnabeled must be a boolean";
+
+        if (this.config.getProperty("webui-enduserLoginEnabled") == null) this.errorMessage = "Property webui-enduserLoginEnabled is missing";
+        if (this.checkBooleanValue(this.config.getProperty("webui-enduserLoginEnabled"))) this.errorMessage = "Property webui-enduserLoginEnabled must be a boolean";
+
+        if (this.config.getProperty("webui-autoLogin") == null) this.errorMessage = "Property webui-autoLogin is missing";
+
+        if (this.config.getProperty("webui-pageChangeAnimation") == null) this.errorMessage = "Property webui-pageChangeAnimation is missing";
+        if (this.checkBooleanValue(this.config.getProperty("webui-pageChangeAnimation"))) this.errorMessage = "Property webui-pageChangeAnimation must be a boolean";
+
+
         if (!this.getErrorMessage().equals(""))
         {
             return;
@@ -171,6 +202,19 @@ public class ApplicationConfigImpl implements ApplicationConfig
         {
             this.configValid = true;
         }
+    }
+
+    //invert-logic in result
+    private boolean checkBooleanValue(String value)
+    {
+        String tmp = value;
+        if (tmp == null) tmp = "";
+        tmp = tmp.toLowerCase().trim();
+
+        if(tmp.equals("true")) return false;
+        if(tmp.equals("false")) return false;
+
+        return true;
     }
 
     public Config getConfig()
